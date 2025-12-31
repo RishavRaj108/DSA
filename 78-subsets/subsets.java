311b3c6dd4f13c7b21cd461b8bbd263790db658a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2025-12-31 13:33:43 +0530

    Time: 1 ms (87.88%) | Memory: 44.3 MB (16.80%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        findAll(0,nums,new ArrayList<>() , result);
+        return result;
+    }
+
+    public void findAll(int ind , int[] nums,ArrayList<Integer> temp ,List<List<Integer>> result){
+        result.add(new ArrayList<>(temp));
+
+        for(int i = ind;i < nums.length;i++){
+            temp.add(nums[i]);
+            findAll(i + 1,nums,temp , result);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
