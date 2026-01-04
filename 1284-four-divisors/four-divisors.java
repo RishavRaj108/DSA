ac08e3873638fa104c3c931406e76f09d81cc9f8
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-01-04 20:46:23 +0530

    Time: 9 ms (97.47%) | Memory: 46.6 MB (33.08%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,41 +1,30 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
-        // return sum of divisors of the integers in that array that have exactly four divisors.
-        // else return 0
-        // find divisors of each element if no is = 4 add to resultant
-        // else leave.
-
-        // how to find divisors of a number.
-        // brute -> from 1 to num start nums[n] % i == 0 divisor Save it to a list.
-        // if(list size == 4 add to res)
-
-        // better approch for divisors
-        // iterate through loop from 1 to num / 2 and add num in the list reduce search space
-
-        int n = nums.length;
         int res = 0;
+        for(int n : nums){
+            // 4 divisors possible only when 
+            // 1 a b ab(n)
+            // 1 a a*a a*a*a
+            int sum = 0;
+            int count = 0;
+            for(int i = 1;i * i <= n;i++){
+                if(n % i == 0){
+                    int d1 = i;
+                    int d2 = n/i;
 
-        for(int i = 0;i < nums.length;i++){ // o(n)
-            List<Integer> ls = new ArrayList<>(); // constant space at max 5
-            int num = nums[i];
-            ls.add(num);
-            for(int j = 1;j <= (num/ 2);j++){ // o(n/2)
-                if(num % j == 0){
-                    ls.add(j);
-                }else{
-                    if(ls.size() > 4){
-                        break;
+                    if(d1 == d2){
+                        count++;
+                        sum += d1;
+                    }else{
+                        count += 2;
+                        sum += d1 + d2;
                     }
+
+                    if(count > 4)break;
                 }
             }
-            if(ls.size() == 4){
-                for(int val : ls){ 
-                    res += val;
-                }
-            }
-        } 
+            if(count == 4) res += sum;
+        }
         return res;
-        // TC: o(n)
-        // sc: o(1)
     }
 }
