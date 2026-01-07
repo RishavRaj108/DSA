6b3a398283cb46e175ee04a5ea8e200d1f9a822c
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-01-07 20:23:51 +0530

    Time: 3 ms (71.35%) | Memory: 44.1 MB (57.87%) - LeetSync

--- 3702-maximum-subarray-with-equal-products/maximum-subarray-with-equal-products.java
+++ 3702-maximum-subarray-with-equal-products/maximum-subarray-with-equal-products.java
@@ -1,32 +1,35 @@
 class Solution {
-    public int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+    
+    public int gcd(int a,int b){
+        if(b == 0)return a;
+        return gcd(b,a%b);
     }
 
-    public int lcm(int a, int b) {
-        return (a / gcd(a, b)) * b;
+    public int lcm(int a,int b){
+        return (a/gcd(a,b)) * b;
     }
 
     public int maxLength(int[] nums) {
-        int n = nums.length;
-        int maxL = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int currGCD = nums[i];
-            int currLCM = nums[i];
-            int currPro = nums[i];
-            
-            for (int j = i + 1; j < n; j++) {
-                currPro *= nums[j];
-                currGCD = gcd(currGCD, nums[j]);
-                currLCM = lcm(currLCM, nums[j]);
-                
-                if (currPro == currLCM * currGCD) {
-                    maxL = Math.max(maxL, j - i + 1);
-                }
+       // prod(arr) == lcm(arr) * gcd(arr)
+       int n = nums.length;
+
+       // gcd => gcd(a,b) => gcd(b,a % b)
+       // lcm => lcm(a,b) => (a/gcd(a,b))*b;
+       int maxLen = 1;
+       for(int i = 0;i < n;i++){
+        int product = nums[i];
+        int g = nums[i];
+        int l = nums[i];
+        for(int j = i + 1;j < n;j++){
+            product = product * nums[j];
+            g = gcd(g, nums[j]);
+            l = lcm(l,nums[j]);
+
+            if(product == (g * l)){
+                maxLen = Math.max(maxLen, j - i + 1);
             }
         }
-        return maxL;
+       } 
+       return maxLen;
     }
 }
-
