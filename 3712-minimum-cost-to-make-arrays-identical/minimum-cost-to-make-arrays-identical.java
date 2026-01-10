88c9fcd29b03fee01acdfc762ee757adce64fb3c
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-01-10 18:56:49 +0530

    Time: 66 ms (66.10%) | Memory: 89.4 MB (13.56%) - LeetSync

--- 3712-minimum-cost-to-make-arrays-identical/minimum-cost-to-make-arrays-identical.java
+++ 3712-minimum-cost-to-make-arrays-identical/minimum-cost-to-make-arrays-identical.java
@@ -1,21 +1,22 @@
 class Solution {
     public long minCost(int[] arr, int[] brr, long k) {
-        int n = arr.length;
-
-        long raw = 0;
-        for(int i = 0;i < n;i++){
+        // return min operations to make arr equal to brr
+        // the best way is to parform operation one and then find the cost to change the values
+         int n = arr.length;
+         long raw = 0;
+         for(int i = 0;i < n;i++){
             raw += Math.abs(arr[i] - brr[i]);
-        }
+         }
 
-        int[] tmp = arr.clone();
-        int[] dist = brr.clone();
+         int[] tmp = arr.clone();
+         int[] dist = brr.clone();
+         Arrays.sort(tmp);
+         Arrays.sort(dist);
 
-        Arrays.sort(tmp);
-        Arrays.sort(dist);
-        long cost = k;
-        for(int i = 0;i < n;i++){
+         long cost = k;
+         for(int i = 0;i < n;i++){
             cost += Math.abs(tmp[i] - dist[i]);
-        }
-        return Math.min(cost, raw);
+         }
+         return Math.min(raw , cost);
     }
 }
