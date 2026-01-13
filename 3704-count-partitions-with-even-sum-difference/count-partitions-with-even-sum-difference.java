3528dbeee7fcd538cb8a5e9cb104e44ae3879193
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-01-13 17:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (93.51%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,24 +1,11 @@
 class Solution {
     public int countPartitions(int[] nums) {
-        // split the array into 2 non empty subarray
-        // return number of partitions 
-        // where the difference between the sum of the left and right subarrays is even.
-        // find the total sum each time we are having sum of subarray then sum of other subsrray is total - sum.
-        // sum1 + sum2 = total
-        // sum1 - sum2 = even
-        int n = nums.length;
         int total = 0;
-        for(int i = 0;i < n;i++){
-           total += nums[i];
+        for(int i = 0;i < nums.length;i++){
+            total += nums[i];
+
         }
-        // for subarray
-        int cnt = 0;
-        int sum = 0;
-        for(int i = 0;i < n - 1;i++){
-            sum += nums[i];
-            int sum2 = total - sum;
-            if((sum2 - sum) % 2 == 0)cnt++;
-        }
-        return cnt;
+        if(total % 2 != 0)return 0;
+        return nums.length - 1;
     }
 }
