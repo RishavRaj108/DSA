348c3b9eccc3d131dd086bd84a91359ee143b73a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-02-04 16:55:14 +0530

    Time: 8 ms (99.59%) | Memory: 42.6 MB (75.56%) - LeetSync

--- 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
@@ -1,14 +1,12 @@
 class Solution {
     public int minAllOneMultiple(int k) {
-        if (k % 2 == 0 || k % 5 == 0) return -1;
+        if(k % 2 == 0 || k % 5 == 0)return -1;
 
-        int remainder = 0;
-
-        for (int len = 1; len <= k; len++) {
-            remainder = (remainder * 10 + 1) % k;
-            if (remainder == 0) return len;
+        int rem = 0;
+        for(int i = 1;i <= k;i++){
+            rem = (rem * 10 + 1) % k;
+            if(rem == 0)return i;
         }
-
         return -1;
     }
 }
