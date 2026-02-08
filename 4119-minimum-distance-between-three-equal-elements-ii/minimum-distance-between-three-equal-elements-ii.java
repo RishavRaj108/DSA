06d4a616974359a7352e6f129ff73838bb495455
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-02-08 11:51:53 +0530

    Time: 105 ms (68.64%) | Memory: 275.2 MB (31.01%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
@@ -1,31 +1,27 @@
 class Solution {
     public int minimumDistance(int[] nums) {
+        Map<Integer, List<Integer>> mp = new HashMap<>();
         int n = nums.length;
-        int ans = Integer.MAX_VALUE;
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
-        // Store all indices for each number
-        for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        // basically here we are creating a map which will store all the occurence of a number 
+        for(int i = 0;i < n;i++){
+            mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
         }
-
-        // Check each list of indices
-        for (List<Integer> list : map.values()) {
-            if (list.size() < 3) continue; // Need at least 3 occurrences
-
-            // Compute distance for first triplet
-            int id1 = list.get(0), id2 = list.get(1), id3 = list.get(2);
-            ans = Math.min(ans, Math.abs(id1 - id2) + Math.abs(id2 - id3) + Math.abs(id3 - id1));
-
-            // Slide the window of size 3 across the list
-            for (int i = 3; i < list.size(); i++) {
-                id1 = id2;
-                id2 = id3;
-                id3 = list.get(i);
-                ans = Math.min(ans, Math.abs(id1 - id2) + Math.abs(id2 - id3) + Math.abs(id3 - id1));
+        
+        int ans = Integer.MAX_VALUE;
+        for(List<Integer> list : mp.values()){
+            if(list.size() < 3)continue;
+            int ind1 = list.get(0);
+            int ind2 = list.get(1);
+            int ind3 = list.get(2);
+            ans = Math.min(ans, Math.abs(ind1 - ind2) + Math.abs(ind2 - ind3) + Math.abs(ind3 - ind1));
+            for(int i = 3;i < list.size();i++){
+                // now the formula
+                ind1 = ind2;
+                ind2 = ind3;
+                ind3 = list.get(i);
+                ans = Math.min(ans, Math.abs(ind1 - ind2) + Math.abs(ind2 - ind3) + Math.abs(ind3 - ind1));
             }
         }
-
-        return ans == Integer.MAX_VALUE ? -1 : ans; // No valid triplet found
+        return ans == Integer.MAX_VALUE? -1 : ans;
     }
 }
