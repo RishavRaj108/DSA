3c9ee1507f4fb2bdeaa0d76c9fc41dd6c2396d21
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-02-09 17:16:49 +0530

    Time: 33 ms (71.34%) | Memory: 132.8 MB (58.48%) - LeetSync

--- 4078-maximum-alternating-sum-of-squares/maximum-alternating-sum-of-squares.java
+++ 4078-maximum-alternating-sum-of-squares/maximum-alternating-sum-of-squares.java
@@ -1,24 +1,25 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
         for(int i = 0;i < n;i++){
-            list.add(Math.abs(nums[i]));
+            nums[i] = Math.abs(nums[i]);
         }
-        Collections.sort(list);
+        Arrays.sort(nums);
         long ans = 0;
         int curOpr = 0;// 0 for large and then conver it to 1 for sub the smaller
-        while(list.size() != 0){
+        int left = 0;
+        int right = n - 1;
+        while(left <= right){
             if(curOpr == 0){
                curOpr = 1;
-               int num = list.get(list.size() - 1);
+               int num = nums[right];
+               right--;
                ans += (long)num * num;
-               list.remove(list.size() - 1);
             }else{
                curOpr = 0;
-               int num = list.get(0);
+               int num = nums[left];
+               left++;
                ans -= (long)num * num;
-               list.remove(0);
             } 
         }
         return ans;
