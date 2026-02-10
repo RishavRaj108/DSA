85348e37c3d44ae674c916e008a532d10078b815
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-02-10 18:08:45 +0530

    Time: 140 ms (48.52%) | Memory: 175.1 MB (96.45%) - LeetSync

---
+++ 4059-design-exam-scores-tracker/design-exam-scores-tracker.java
@@ -0,0 +1,26 @@
+class ExamTracker {
+
+    List<Long> pre = new ArrayList<>(List.of(0L));
+    List<Long> times = new ArrayList<>(List.of(0L));
+
+    void record(long time, long score) {
+        pre.add(pre.get(pre.size() - 1) + score);
+        times.add(time);
+    }
+
+    long totalScore(long startTime, long endTime) {
+        int i = Collections.binarySearch(times, startTime);
+        if (i < 0) i = -i - 1;
+        int j = Collections.binarySearch(times, endTime);
+        if (j < 0) j = -j - 1;
+        else j++;
+        return pre.get(j - 1) - pre.get(i - 1);
+    }
+}
+
+/**
+ * Your ExamTracker object will be instantiated and called as such:
+ * ExamTracker obj = new ExamTracker();
+ * obj.record(time,score);
+ * long param_2 = obj.totalScore(startTime,endTime);
+ */
