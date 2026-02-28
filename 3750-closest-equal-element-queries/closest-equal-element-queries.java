e6cff691967e9727cb36b77984f3d6784011a45c
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-02-28 19:06:57 +0530

    Time: 51 ms (96.00%) | Memory: 188.7 MB (32.00%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -1,47 +1,40 @@
 class Solution {
     public List<Integer> solveQueries(int[] nums, int[] queries) {
-        int n = nums.length;
-        
-        // Step 1: store all indices of each value
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
-        }
-        
-        int[] ans = new int[n];
-        Arrays.fill(ans, -1);
-        
-        // Step 2: process each value group
-        for (List<Integer> list : map.values()) {
-            if (list.size() == 1) continue;  // only one occurrence
+       Map<Integer , List<Integer>> mp = new HashMap<>();
+       int n = nums.length;
+       for(int i = 0;i < n;i++){
+        mp.computeIfAbsent(nums[i] , k -> new ArrayList<>()).add(i);
+       } 
+
+       int[] ans = new int[n];
+       Arrays.fill(ans , -1);
+       
+       for(List<Integer> ls : mp.values()){
+        if(ls.size() == 1)continue;
+        int size = ls.size();
+        // 0 
+        for(int i = 0;i < size;i++){
+            int curr = ls.get(i);
+            int next = ls.get((i + 1)% size);
             
-            int size = list.size();
-            
-            for (int i = 0; i < size; i++) {
-                int curr = list.get(i);
-                int next = list.get((i + 1) % size);
-                
-                int dist = (next - curr + n) % n;
-                
-                if (ans[curr] == -1)
-                    ans[curr] = dist;
-                else
-                    ans[curr] = Math.min(ans[curr], dist);
-                    
-                if (ans[next] == -1)
-                    ans[next] = dist;
-                else
-                    ans[next] = Math.min(ans[next], dist);
+            int diff = (next - curr + n) % n;
+            if(ans[curr] == -1){
+                ans[curr] = diff;
+            }else{
+                ans[curr] = Math.min(diff , ans[curr]);
+            }
+            if(ans[next] == -1){
+                ans[next] = diff;
+            }else{
+                ans[next] = Math.min(diff , ans[next]);
             }
         }
-        
-        // Step 3: answer queries
-        List<Integer> result = new ArrayList<>();
-        for (int q : queries) {
-            result.add(ans[q]);
-        }
-        
-        return result;
+       }
+       int m = queries.length;
+       List<Integer> res = new ArrayList<>();
+       for(int i = 0;i < m;i++){
+         res.add(ans[queries[i]]);
+       }
+       return res;
     }
 }
