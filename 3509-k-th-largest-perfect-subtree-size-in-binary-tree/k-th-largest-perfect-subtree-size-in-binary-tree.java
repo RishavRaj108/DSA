ab8db3c65589ab1647e538da4ff8da679326923a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-07 16:49:00 +0530

    Time: 19 ms (21.79%) | Memory: 47.1 MB (86.79%) - LeetSync

--- 3509-k-th-largest-perfect-subtree-size-in-binary-tree/k-th-largest-perfect-subtree-size-in-binary-tree.java
+++ 3509-k-th-largest-perfect-subtree-size-in-binary-tree/k-th-largest-perfect-subtree-size-in-binary-tree.java
@@ -15,60 +15,31 @@
  */
 class Solution {
     class Pair{
-        int node;
-        boolean valid;
-        Pair(int node , boolean valid){
-            this.node = node;
-            this.valid = valid;
+        int size;
+        boolean perfect;
+        Pair(int size , boolean perfect){
+            this.size = size;
+            this.perfect = perfect;
         }
     }
     public int kthLargestPerfectSubtree(TreeNode root, int k) {
-        // traverse the tree dfs pass list where we are going to store the perfect tree size
-        // in dfs traversal two parametrs left size and right size the moment both equal add it to list
         List<Integer> ls = new ArrayList<>();
-        dfs(root,true , ls);
-        Collections.sort(ls);
-        if(ls.size() < k)return  -1;
-        return ls.get(ls.size() - k);
-    }
-    public Pair dfs(TreeNode node,boolean valid , List<Integer> ls){
-        if(node.left == null && node.right == null){
-            ls.add(1);
-           return new Pair(1 , true);
-        } 
-        int leftPart = 0;
-        boolean check = false;
-        if(node.left != null){
-            Pair leftPa = dfs(node.left ,valid , ls);
-            leftPart = leftPa.node;
-            check = leftPa.valid;
+
+        dfs(root , ls);
+        Collections.sort(ls , Collections.reverseOrder());
+        if(ls.size() < k)return -1;
+        return ls.get(k - 1);
+    } 
+    public Pair dfs(TreeNode node , List<Integer> ls){
+        if(node == null)return new Pair(0,true);
+
+        Pair left = dfs(node.left , ls);
+        Pair right = dfs(node.right , ls);
+        if(left.size == right.size && left.perfect && right.perfect){
+            int size = left.size + right.size + 1;
+            ls.add(size);
+            return new Pair(size , true);
         }
-        int rightPart = 0;
-        if(node.right != null){
-            Pair rightPa = dfs(node.right ,valid , ls);
-            rightPart = rightPa.node;
-            check = check == true? rightPa.valid : false;
-        }
-        int cnt = rightPart + leftPart + 1;
-        if(leftPart == rightPart && check){
-            ls.add(cnt);
-            return new Pair(cnt , true);
-        }
-        return new Pair(cnt , false); 
+        return new Pair(left.size + right.size + 1 , false);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
