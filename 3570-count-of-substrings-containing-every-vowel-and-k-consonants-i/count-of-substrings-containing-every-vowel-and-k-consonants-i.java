1a09cf3c129ad7365092a421766f6bc4d99bba17
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-08 10:55:55 +0530

    Time: 6 ms (67.11%) | Memory: 44.4 MB (68.42%) - LeetSync

---
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countOfSubstrings(String word, int k) {
+        return atLeast(word, k) - atLeast(word, k + 1);
+    }
+    
+    private int atLeast(String word, int k) {
+        Map<Character, Integer> vowels = new HashMap<>();
+        int left = 0, consonants = 0, count = 0;
+        int n = word.length();
+        
+        for (int right = 0; right < n; right++) {
+            char ch = word.charAt(right);
+            if ("aeiou".indexOf(ch) >= 0) {
+                vowels.merge(ch, 1, Integer::sum);
+            } else {
+                consonants++;
+            }
+            
+            // Shrink while window is valid (has all vowels AND >= k consonants)
+            while (vowels.size() == 5 && consonants >= k) {
+                count += (n - right);  // All extensions of this window are valid
+                char leftChar = word.charAt(left);
+                if ("aeiou".indexOf(leftChar) >= 0) {
+                    vowels.merge(leftChar, -1, Integer::sum);
+                    if (vowels.get(leftChar) == 0) vowels.remove(leftChar);
+                } else {
+                    consonants--;
+                }
+                left++;
+            }
+        }
+        return count;
+    }
+}
