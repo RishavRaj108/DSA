1a896216db144a85b51f75d83f501e803be649b3
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-10 18:53:23 +0530

    Time: 28 ms (33.88%) | Memory: 47.2 MB (30.62%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -1,54 +1,47 @@
 class Solution {
-    public int m , n;
-    boolean[][] vis;
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
-        // up , down , right , left 
-        // till health remains positive
-        // grid[i][j] == 1 reduce health by 1
-        // return true if it is possible to reach 
-         m = grid.size() ;
-         n = grid.get(0).size();
-        int[][][] dp = new int[m + 1][n + 1][health + 1];
-        for(int[][] arr : dp){
-            for(int[] row : arr){
-                Arrays.fill(row , -1);
+        // just go for bfs approch
+        int m = grid.size();
+        int n = grid.get(0).size();
+
+        int[][] best = new int[m][n];
+        for(int[] row : best){
+            Arrays.fill(row , - 1);
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        int start = health - grid.get(0).get(0);
+        if(start <= 0) return false;
+
+        q.offer(new int[]{0,0,start});
+
+        best[0][0] = start;
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
+            int h = cur[2];
+
+            if(r == m - 1 && c == n - 1)return true;
+
+            for(int[] d: dirs){
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= m || nc >= n || nr < 0 || nc < 0)continue;
+
+                int newHeal = h - grid.get(nr).get(nc);
+                if(newHeal <= 0)continue;
+
+                if(best[nr][nc] >= newHeal)continue;
+
+                best[nr][nc] = newHeal;
+                q.offer(new int[]{nr,nc ,newHeal});
             }
-        }
-        vis = new boolean[m][n];
-        return find(0,0 , grid , health , dp);
-    }
-
-    public boolean find(int row , int col ,List<List<Integer>> grid, int health , int[][][] dp){
-        if(row == m - 1 && col == n - 1 )return health > grid.get(row).get(col);
-        if(row >= m || col >= n || row < 0 || col < 0 || health <= 0 || vis[row][col] ){
-            return false;
-        }
-
-        // now i can move in four directions
-        
-
-        int val = health;
-        if(grid.get(row).get(col) == 1){
-            val--;
-        }
-        if(dp[row][col][val] != -1){
-            if(dp[row][col][val] == 0){
-                return false;
-            }else{
-                return true;
-            }
-        }
-        vis[row][col] = true;
-        boolean top = find(row - 1, col , grid, val , dp);
-        boolean down = find(row + 1 , col , grid, val,dp);
-        boolean left = find(row , col - 1,grid , val,dp);
-        boolean right = find(row , col + 1 , grid , val,dp);
-        vis[row][col] = false;
-        if(top || right || left || down){
-            dp[row][col][val] = 1;
-            return true;
-        }
-        dp[row][col][val] = 0;
+        } 
         return false;
-    } 
+    }
 }
