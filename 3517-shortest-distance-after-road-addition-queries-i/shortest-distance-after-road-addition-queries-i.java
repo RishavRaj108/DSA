ccd2e5682310a56d71bc9702343b784239d3737f
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-15 16:30:50 +0530

    Time: 115 ms (50.00%) | Memory: 47.5 MB (73.91%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,45 +1,40 @@
 class Solution {
-    public int dijkstra(int n , List<List<int[]>> adj){
-        // for dijkstra stuff needed
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-
-        int[] dist = new int[n];
-        Arrays.fill(dist , Integer.MAX_VALUE);
-
-        dist[0] = 0;
-        q.add(new int[]{0 , 0});
-
+    public int bfs(int n , Map<Integer , List<Integer>> adj){
+        Queue<Integer> q = new LinkedList<>();
+        // we don't want to add already visited node to queue so we maintain a visited array
+        boolean[] visited = new boolean[n];
+        q.add(0);// node
+        visited[0] = true;
+        int level = 0;
         while(!q.isEmpty()){
-            int[] t = q.poll();
-            int u = t[1];
-            int d = t[0];
-            if(u == n - 1)return d;
-            if(dist[u] < d)continue;
-            
-            for(int[] neig : adj.get(u)){
-                int v = neig[0];
-                int dis = neig[1];
-                if(dist[v] > d + dis){
-                    dist[v] = d + dis;
-                    q.add(new int[]{d + dis , v});
+            int size = q.size();
+            for(int i = 0;i < size;i++){
+                int node = q.poll();
+                if(node == n - 1)return level;
+
+                for(int neig : adj.get(node)){
+                    if(visited[neig] == false){
+                       visited[neig] = true;
+                       q.add(neig);
+                    }
                 }
             }
+            level++;
         }
         return -1;
     }
-    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0;i < n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i = 0;i < n - 1;i++){
-            adj.get(i).add(new int[]{i + 1, 1});
-        }
 
-        int[] res = new int[queries.length];
-        for(int i = 0;i < queries.length;i++){
-            adj.get(queries[i][0]).add(new int[]{queries[i][1],1});
-            res[i] = dijkstra(n , adj);
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        Map<Integer , List<Integer>> adj = new HashMap<>();
+
+        for(int i = 0;i < n - 1;i++){
+            adj.computeIfAbsent(i , k -> new ArrayList<>()).add(i + 1);
+        }
+        int m = queries.length;
+        int[] res = new int[m];
+        for(int i = 0;i < m;i++){
+            adj.computeIfAbsent(queries[i][0] , k -> new ArrayList<>()).add(queries[i][1]);
+            res[i] = bfs(n , adj);
         }
         return res;
     }
