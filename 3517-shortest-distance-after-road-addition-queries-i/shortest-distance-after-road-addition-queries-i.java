df9e71a8a29d7c2ce19ff44f6654ae59d586f553
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-15 14:41:57 +0530

    Time: 163 ms (39.13%) | Memory: 47.3 MB (87.83%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,45 +1,46 @@
 class Solution {
+    public int dijkstra(int n , List<List<int[]>> adj){
+        // for dijkstra stuff needed
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[0] - b[0]);
 
-    public int bfs(int n , List<List<Integer>> adj){
-        Queue<Integer> q = new LinkedList<>();
-        int level = 0;
-        q.add(0);
-        boolean[] vis = new boolean[n];
-        vis[0] = true;
+        int[] dist = new int[n];
+        Arrays.fill(dist , Integer.MAX_VALUE);
+
+        dist[0] = 0;
+        q.add(new int[]{0 , 0});
+
         while(!q.isEmpty()){
-            int size = q.size();
+            int[] t = q.poll();
+            int u = t[1];
+            int d = t[0];
+            if(u == n - 1)return d;
+            if(dist[u] < d)continue;
             
-            for(int i = 0;i < size;i++){
-                int node = q.poll();
-
-                if(node == n - 1)return level;
-                
-                for(int neig : adj.get(node)){
-                    if(vis[neig] == false){
-                      q.add(neig);
-                      vis[neig] = true;
-                    }
+            for(int[] neig : adj.get(u)){
+                int v = neig[0];
+                int dis = neig[1];
+                if(dist[v] > d + dis){
+                    dist[v] = d + dis;
+                    q.add(new int[]{d + dis , v});
                 }
             }
-            level++;
         }
         return -1;
     }
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        List<List<Integer>> adj = new ArrayList<>();
-      
+        List<List<int[]>> adj = new ArrayList<>();
         for(int i = 0;i < n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i = 0;i <= n - 2;i++){
-            adj.get(i).add(i + 1);
+        for(int i = 0;i < n - 1;i++){
+            adj.get(i).add(new int[]{i + 1, 1});
         }
 
         int[] res = new int[queries.length];
         for(int i = 0;i < queries.length;i++){
-            adj.get(queries[i][0]).add(queries[i][1]);
-            res[i] = bfs(n , adj);
+            adj.get(queries[i][0]).add(new int[]{queries[i][1],1});
+            res[i] = dijkstra(n , adj);
         }
-      return res;
+        return res;
     }
 }
