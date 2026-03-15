6cdc4b8410ce837faff68de21be8f3f49f28a47d
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-15 17:20:13 +0530

    Time: 191 ms (31.30%) | Memory: 47.6 MB (60.00%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,40 +1,43 @@
 class Solution {
-    public int bfs(int n , Map<Integer , List<Integer>> adj){
-        Queue<Integer> q = new LinkedList<>();
-        // we don't want to add already visited node to queue so we maintain a visited array
-        boolean[] visited = new boolean[n];
-        q.add(0);// node
-        visited[0] = true;
-        int level = 0;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0;i < size;i++){
-                int node = q.poll();
-                if(node == n - 1)return level;
+    public int dijkstra(int n , Map<Integer , List<int[]>> adj ){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-                for(int neig : adj.get(node)){
-                    if(visited[neig] == false){
-                       visited[neig] = true;
-                       q.add(neig);
-                    }
+        int[] dist = new int[n];
+        Arrays.fill(dist , Integer.MAX_VALUE);
+
+        pq.add(new int[]{ 0 , 0});
+        dist[0] = 0;
+
+        while(!pq.isEmpty()){
+            int[] t = pq.poll();
+            int d = t[0];
+            int u = t[1];
+
+            if(u == n - 1)return d;
+            if(dist[u] < d)continue;
+
+            for(int[] neig : adj.get(u)){
+                int dis = neig[1];
+                int v = neig[0];
+
+                if(dist[v] > d + dis){
+                    dist[v] = d + dis;
+                    pq.add(new int[]{dist[v] , v});
                 }
             }
-            level++;
         }
         return -1;
-    }
-
+     }
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        Map<Integer , List<Integer>> adj = new HashMap<>();
-
+        Map<Integer , List<int[]>> adj = new HashMap<>();
         for(int i = 0;i < n - 1;i++){
-            adj.computeIfAbsent(i , k -> new ArrayList<>()).add(i + 1);
+            adj.computeIfAbsent(i , k -> new ArrayList<>()).add(new int[]{i + 1 , 1});
         }
         int m = queries.length;
         int[] res = new int[m];
         for(int i = 0;i < m;i++){
-            adj.computeIfAbsent(queries[i][0] , k -> new ArrayList<>()).add(queries[i][1]);
-            res[i] = bfs(n , adj);
+            adj.computeIfAbsent(queries[i][0] , k -> new ArrayList<>()).add(new int[]{queries[i][1] , 1});
+            res[i] = dijkstra(n , adj);
         }
         return res;
     }
