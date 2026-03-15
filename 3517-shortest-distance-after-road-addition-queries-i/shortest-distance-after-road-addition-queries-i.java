1feeb7083ed99a4856ed14fc9bd19b1843759b0a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-15 14:01:04 +0530

    Time: 100 ms (60.87%) | Memory: 47.2 MB (96.52%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,51 +1,45 @@
 class Solution {
-    private Map<Integer, List<Integer>> adj = new HashMap<>();
 
-    private int bfs(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-        queue.offer(0);
-        visited[0] = true;
-
+    public int bfs(int n , List<List<Integer>> adj){
+        Queue<Integer> q = new LinkedList<>();
         int level = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                int node = queue.poll();
+        q.add(0);
+        boolean[] vis = new boolean[n];
+        vis[0] = true;
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            for(int i = 0;i < size;i++){
+                int node = q.poll();
 
-                if (node == n - 1) {
-                    return level; // Found the destination node
-                }
-
-                for (int neighbor : adj.getOrDefault(node, new ArrayList<>())) {
-                    if (!visited[neighbor]) {
-                        queue.offer(neighbor);
-                        visited[neighbor] = true;
+                if(node == n - 1)return level;
+                
+                for(int neig : adj.get(node)){
+                    if(vis[neig] == false){
+                      q.add(neig);
+                      vis[neig] = true;
                     }
                 }
             }
             level++;
         }
-        return -1; // If destination node is unreachable
+        return -1;
     }
-
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        adj.clear();
-
-        // Initialize adjacency list with direct edges (i -> i + 1)
-        for (int i = 0; i < n - 1; i++) {
-            adj.computeIfAbsent(i, k -> new ArrayList<>()).add(i + 1);
+        List<List<Integer>> adj = new ArrayList<>();
+      
+        for(int i = 0;i < n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0;i <= n - 2;i++){
+            adj.get(i).add(i + 1);
         }
 
-        int k = queries.length;
-        int[] res = new int[k];
-        for (int i = 0; i < k; i++) {
-            int u = queries[i][0];
-            int v = queries[i][1];
-            adj.computeIfAbsent(u, k1 -> new ArrayList<>()).add(v);
-            res[i] = bfs(n);
+        int[] res = new int[queries.length];
+        for(int i = 0;i < queries.length;i++){
+            adj.get(queries[i][0]).add(queries[i][1]);
+            res[i] = bfs(n , adj);
         }
-
-        return res;
+      return res;
     }
 }
