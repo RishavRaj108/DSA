3dadb12c6826aa9b4b71be91003d94563bb13877
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-19 20:30:48 +0530

    Time: 5 ms (98.11%) | Memory: 45.5 MB (45.28%) - LeetSync

--- 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.java
+++ 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.java
@@ -2,29 +2,23 @@ class Solution {
     public int minimumAddedInteger(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-
         int ans = Integer.MAX_VALUE;
-
-        // Try removing nums1[0], nums1[1], nums1[2] possibilities
-        for (int i = 0; i < 3; i++) {
-            int x = nums2[0] - nums1[i];
-
+        for(int i = 0;i < 3;i++){
             int skip = 0;
-            int j = 0;
-
-            for (int k = 0; k < nums1.length; k++) {
-                if (j < nums2.length && nums1[k] + x == nums2[j]) {
-                    j++;
-                } else {
+            int diff = nums2[0] - nums1[i];
+            int k = 0;
+            for(int j =0;j < nums1.length;j++){
+                if(k < nums2.length && nums2[k] - diff == nums1[j]){
+                    k++;
+                }else{
                     skip++;
                 }
             }
 
-            if (skip == 2) {
-                ans = Math.min(ans, x);
+            if(skip == 2){
+                ans = Math.min(ans , diff);
             }
         }
-
         return ans;
     }
 }
