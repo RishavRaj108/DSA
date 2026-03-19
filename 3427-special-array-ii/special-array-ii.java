2739663314cb1e493b7b97d54d752adc6e97c99f
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-19 18:31:06 +0530

    Time: 3 ms (66.46%) | Memory: 120.6 MB (22.15%) - LeetSync

--- 3427-special-array-ii/special-array-ii.java
+++ 3427-special-array-ii/special-array-ii.java
@@ -4,24 +4,27 @@ class Solution {
         int m = queries.length;
 
         boolean[] res = new boolean[m];
-        int[] pref = new int[n];
 
-        for(int i = 1;i < n;i++){
-            // for each index we will check if its parity is diffrent from prev index
-            int parity = nums[i] % 2;
-            if(nums[i - 1] % 2 == parity){
-                pref[i] = pref[i - 1] + 1;
-            }else{
-                pref[i] = pref[i - 1];
+        // for each index i have to find the max safe stop it can travel
+        int[] canVisit = new int[n];
+        int i = 0;
+        int j = 0;
+        while(i < n){
+            if(j < i){
+                j = i;
             }
-
+            while(j + 1 < n && nums[j + 1] % 2 != nums[j] % 2){
+                j++;
+            }
+            canVisit[i] = j;
+            i++;
         }
-
         int ind = 0;
         for(int[] quer : queries){
             int start = quer[0];
             int end = quer[1];
-            if(pref[start] == pref[end]){
+
+            if(canVisit[start] >= end){
                 res[ind++] = true;
             }else{
                 res[ind++] = false;
