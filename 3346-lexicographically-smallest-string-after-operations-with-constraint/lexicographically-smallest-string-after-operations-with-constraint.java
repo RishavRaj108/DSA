d7ec97e2f4707e1d5fbe9e2cddb49a859457e2d4
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-20 23:36:33 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (45.61%) - LeetSync

---
+++ 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String getSmallestString(String s, int k) {
+        if(k == 0)return s;
+
+        // lexico that means every time we aim to make first index to as small as possible
+        String str = "abcdefghijklmnopqrstuvwxyz";
+
+        char[] chars = s.toCharArray();
+
+        int ind = 0;
+        while( k > 0 && ind < chars.length){
+            // try to reduce the index to as low as possible
+            char ch = chars[ind];
+            int val = Math.min(ch - 'a' , Math.abs(ch - 'z' )+ 1);
+            if(val <= k){
+               chars[ind] = 'a';
+               k -= val;
+            }else{
+               val = str.indexOf(ch);
+               chars[ind] = str.charAt(val - k);
+               break;
+            }
+            ind++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char ch : chars){
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
