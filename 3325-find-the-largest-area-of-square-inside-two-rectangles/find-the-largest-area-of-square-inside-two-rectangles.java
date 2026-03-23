2d8f8124371c7e6576fcd10c08b8722d1a732238
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-23 22:52:04 +0530

    Time: 79 ms (39.70%) | Memory: 47.9 MB (50.56%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
@@ -1,19 +1,38 @@
 class Solution {
     public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        // bottom left is the point of rectangle from this we get smallest x and smallest y for that specific rectangle
+        // top right is the point of rectangle from this we get largest x and largest y that this rectangle has
+        
+        // now thaught process we have to find the largest square that exist in two rectangle
+
+        // now how to find intersection 
+        // we have min x and min y of both the rectangle
+        // we are going to choose the maximum
+        // similarly for maxi x and y we are going to choose the smallest
+        long side = 0;
         int n = bottomLeft.length;
-        int side = 0;
         for(int i = 0;i < n;i++){
             for(int j = i + 1;j < n;j++){
-               // find from bottom max and from top min
-               int maxX = Math.max(bottomLeft[i][0],bottomLeft[j][0]);
-               int maxY = Math.max(bottomLeft[i][1],bottomLeft[j][1]);
-               int minX = Math.min(topRight[i][0],topRight[j][0]);
-               int minY = Math.min(topRight[i][1],topRight[j][1]);
-               if(minX > maxX && minY > maxY){
-                  side = Math.max(side, Math.min(minX - maxX , minY - maxY));
-               }
+                int maxx = Math.max(bottomLeft[i][0]  , bottomLeft[j][0]);
+                int minx = Math.min(topRight[i][0]  , topRight[j][0]);
+                int maxy = Math.max(bottomLeft[i][1]  , bottomLeft[j][1]);
+                int miny = Math.min(topRight[i][1]  , topRight[j][1]);
+                if(minx > maxx && miny > maxy){
+                    side = Math.max(side , Math.min(minx - maxx , miny - maxy));
+                }
+
             }
         }
-        return (long)side * side;
+        return 1L * side * side;
     }
 }
+
+
+
+
+
+
+
+
+
+
