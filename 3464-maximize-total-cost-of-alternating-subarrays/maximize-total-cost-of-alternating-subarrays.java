dba4562d8bbccf4c04a593fddb036c85bfa9228f
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-26 12:05:28 +0530

    Time: 44 ms (6.17%) | Memory: 139.8 MB (7.41%) - LeetSync

--- 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.java
+++ 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.java
@@ -1,20 +1,19 @@
 class Solution {
     Long[][] dp;
-
     public long maximumTotalCost(int[] nums) {
-        dp = new Long[nums.length][2];
-        return solve(0, 1, nums);
+     // now for dp part
+     int n = nums.length;
+     dp = new Long[n][2];
+     return solve(0,1,nums);   
     }
-
-    private long solve(int i, int sign, int[] nums) {
-        if (i == nums.length) return 0;
-
-        int s = (sign == 1) ? 0 : 1;
-        if (dp[i][s] != null) return dp[i][s];
-
-        long take = nums[i] * sign + solve(i + 1, -sign, nums);
-        long restart = nums[i] + solve(i + 1, -1, nums);
-
-        return dp[i][s] = Math.max(take, restart);
+    public long solve(int ind , int sign , int[] nums){
+        if(ind == nums.length)return 0;
+        int s = sign == 1? 1 : 0;
+        if(dp[ind][s] != null)return dp[ind][s];
+        // take current
+        long take = nums[ind] * sign + solve(ind + 1, -sign,nums);
+        // not take current and start a new subarray
+        long notake= nums[ind] + solve(ind + 1, -1,nums);
+        return dp[ind][s] = Math.max(take , notake);
     }
 }
