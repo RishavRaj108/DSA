33ddcf79cc891b73d0f61876ddd217a8c9cc414c
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-28 22:31:02 +0530

    Time: 3 ms (92.44%) | Memory: 97.4 MB (84.87%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,28 +1,25 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
         int n = colors.length;
-        int[] extended = new int[n + k - 1];
+        int[] extend = new int[n + k - 1];
         for(int i = 0;i < n;i++){
-            extended[i] = colors[i];
+            extend[i] = colors[i];
         }
         for(int i = 0;i < k - 1;i++){
-            extended[i + n] = colors[i];
+            extend[i + n] = colors[i];
         }
 
-        int cnt = 0;
+        // now normal sliding window
         int left = 0;
-        for(int right = 1;right < extended.length;right++){
-            // now check if it is valid or not 
-            if(extended[right] == extended[right - 1]){
-                left = right;
-                continue;
-            }
-
-            if(right - left + 1 == k){
-                cnt++;
-                left++;
-            }
-
+        int cnt = 0;
+        for(int right = 1;right < extend.length;right++){
+           if(extend[right - 1] == extend[right]){
+            left = right;
+           }
+           if(right - left + 1 == k){
+            cnt++;
+            left++;
+           }
         }
         return cnt;
     }
