1b735d4ea7613d8e0eb5a216f4b652999f66318a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-29 12:00:20 +0530

    Time: 333 ms (41.86%) | Memory: 261.4 MB (43.02%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.java
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.java
@@ -1,23 +1,30 @@
 class Solution {
-    public int maxTotalReward(int[] rewardVal) {
-        int n = rewardVal.length;
-        Arrays.sort(rewardVal);
-        int[][] dp = new int[n][2000 + 2000];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
-        }
-        return solve(0,0,rewardVal , dp);
-    }
-    public int solve(int ind , int total , int[] vals , int[][] dp){
-        if(ind >= vals.length)return 0;
+    public int maxTotalReward(int[] rewardValues) {
+        Arrays.sort(rewardValues);
 
-        if(dp[ind][total] != -1)return dp[ind][total];
+        int n = rewardValues.length;
+        int maxSum = 4000;
+
+        int[][] dp = new int[n][maxSum + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        return solve(0, 0, rewardValues, dp);
+    }
+
+    private int solve(int i, int sum, int[] vals, int[][] dp) {
+        if (i == vals.length) return sum;
+
+        if (dp[i][sum] != -1) return dp[i][sum];
+
+        // skip
+        int notTake = solve(i + 1, sum, vals, dp);
+
+        // take (only if valid)
         int take = 0;
-        // now take and notake possibltiy
-        if(vals[ind] > total){
-            take = vals[ind] + solve(ind + 1 , total + vals[ind] , vals, dp);
+        if (vals[i] > sum) {
+            take = solve(i + 1, sum + vals[i], vals, dp);
         }
-        int notake = solve(ind + 1, total , vals , dp);
-        return dp[ind][total] = Math.max(take , notake);
+
+        return dp[i][sum] = Math.max(take, notTake);
     }
 }
