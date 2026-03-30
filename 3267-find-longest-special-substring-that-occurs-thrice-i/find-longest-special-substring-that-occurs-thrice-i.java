4bec20a0e9265576f2aaee33992db75636bbe895
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-03-30 10:49:40 +0530

    Time: 4 ms (79.50%) | Memory: 45.4 MB (81.59%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,30 +1,46 @@
 class Solution {
     public int maximumLength(String s) {
         int n = s.length();
-        Map<String ,Integer> mp = new HashMap<>();
+        
+        // For each char, store list of block lengths
+        List<Integer>[] groups = new ArrayList[26];
+        for (int i = 0; i < 26; i++) {
+            groups[i] = new ArrayList<>();
+        }
 
-        for(int i = 0;i < n;i++){
-            for(int j = i;j < n;j++){
-                String str = s.substring(i , j + 1);
-                mp.put(str, mp.getOrDefault(str , 0) + 1);
+        // Step 1: build blocks
+        int i = 0;
+        while (i < n) {
+            int j = i;
+            while (j < n && s.charAt(j) == s.charAt(i)) j++;
+            
+            int len = j - i;
+            groups[s.charAt(i) - 'a'].add(len);
+            i = j;
+        }
+
+        int ans = -1;
+
+        // Step 2: process each character
+        for (int c = 0; c < 26; c++) {
+            List<Integer> list = groups[c];
+            if (list.isEmpty()) continue;
+
+            // Sort descending
+            Collections.sort(list, Collections.reverseOrder());
+
+            // Try top 3 largest blocks
+            for (int k = 1; k <= list.get(0); k++) {
+                int count = 0;
+                for (int len : list) {
+                    if (len >= k) count += (len - k + 1);
+                }
+                if (count >= 3) {
+                    ans = Math.max(ans, k);
+                }
             }
         }
 
-        int maxi = -1;
-        for(String str : mp.keySet()){
-            int count = mp.get(str);
-            if(count >= 3 && isSpecial(str)){
-                maxi = Math.max(maxi , str.length());
-            }
-        }   
-        return maxi;
-    }
-    public boolean isSpecial(String s){
-        for(int i = 1;i < s.length();i++){
-          if(s.charAt(i) != s.charAt(i - 1)){
-            return false;
-          }
-        }
-        return true;
+        return ans;
     }
 }
