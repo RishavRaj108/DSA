b9c0709176ab49e95e0bb56e648faf180fe0aa27
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-04 20:07:25 +0530

    Time: 379 ms (85.71%) | Memory: 169.7 MB (11.11%) - LeetSync

--- 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.java
+++ 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public long numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        long cnt = 0;
         Map<Integer, Integer> mp = new HashMap<>();
         for(int num : nums2){
             mp.put(num , mp.getOrDefault(num , 0) + 1);
         }
 
-        long ans = 0;
-        for(int i = 0;i < nums1.length;i++){
-            int num = nums1[i];
+        for(int num : nums1){
             if(num % k != 0)continue;
-
             int val = num / k;
-            // now this val must be divisible by nums2 numbers
-            for(int d = 1;d * d <= val;d++){
-                if(val % d == 0){
-                    if(mp.containsKey(d)){
-                        ans += mp.get(d);
+
+            for(int i = 1;i * i <= val;i++){
+                if(val % i == 0 ){
+                    if(mp.containsKey(i)){
+                      cnt += mp.get(i);
                     }
-                    int other = val / d;
-                    if(other != d && mp.containsKey(other)){
-                        ans += mp.get(other);
+                    int other = val / i;
+                    if(other != i && mp.containsKey(other)){
+                        cnt += mp.get(other);
                     }
+                    
                 }
             }
         }
-        return ans;
+        return cnt;
     }
 }
