03edc39335d9165df78d9ff21f75b2e79e8661c5
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-05 11:58:50 +0530

    Time: 68 ms (69.92%) | Memory: 183.7 MB (58.65%) - LeetSync

--- 3363-most-frequent-ids/most-frequent-ids.java
+++ 3363-most-frequent-ids/most-frequent-ids.java
@@ -1,22 +1,21 @@
 class Solution {
     public long[] mostFrequentIDs(int[] nums, int[] freq) {
-        Map<Long , Long> mp = new HashMap<>();
-
-        PriorityQueue<Long[]> pq = new PriorityQueue<>((a,b) -> Long.compare(b[0] , a[0]));
-
+        
         int n = nums.length;
-        long[] result = new long[n];
+        Map<Long,Long> mp = new HashMap<>();
+
+        long[] res = new long[n];
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b) -> Long.compare(b[0] ,a[0]));
         for(int i = 0;i < n;i++){
             long num = nums[i];
             long f = freq[i];
-            
             mp.put(num , mp.getOrDefault(num , 0L) + f);
-            pq.add(new Long[]{mp.get(num) , num});
-            while(!pq.isEmpty() && mp.get(pq.peek()[1]) != pq.peek()[0]){
+            pq.add(new long[] {mp.get(num) , num});
+            while(!pq.isEmpty() && pq.peek()[0] != mp.get(pq.peek()[1])){
                 pq.poll();
             }
-            result[i] = pq.peek()[0];
+            res[i] = pq.peek()[0];
         }
-        return result;
+        return res;
     }
 }
