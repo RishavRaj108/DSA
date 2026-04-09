69d827ca3ee3242a0fd4136d45d3cbf41e497b27
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-09 09:39:07 +0530

    Time: 40 ms (85.00%) | Memory: 65.8 MB (93.75%) - LeetSync

--- 3104-happy-students/happy-students.java
+++ 3104-happy-students/happy-students.java
@@ -1,22 +1,31 @@
 class Solution {
     public int countWays(List<Integer> nums) {
+        // task is to make all student happy
+        // if selected students > nums[i] then we can select this std
+        // if selected student < nums[i] then not selected
+        // if at any spot no of elements including it > nums[i] this is selection block but next should not be selected
+
+        // we can select any group present in nums in any order
+
         Collections.sort(nums);
         int n = nums.size();
-        int count = 0;
 
-        // k = 0 case
-        if (nums.get(0) > 0) count++;
-
-        // k = 1 to n-1
-        for (int k = 1; k < n; k++) {
-            if (nums.get(k - 1) < k && nums.get(k) > k) {
-                count++;
-            }
+        int cnt = 0;
+        if(nums.get(0) > cnt){
+            cnt++;
         }
+        // 0 2 3 3 6 6 7 7
+        // t std > nums[i] then i selected
+        // t std < nums[i] then i not selected
+        for(int i = 1;i < n;i++){
+           if(nums.get(i - 1) < i && nums.get(i) > i){
+            cnt++;
+           }
+        }
+        if(nums.get(n - 1) < n){
+            cnt++;
+        }
+        return cnt;
 
-        // k = n case
-        if (nums.get(n - 1) < n) count++;
-
-        return count;
     }
 }
