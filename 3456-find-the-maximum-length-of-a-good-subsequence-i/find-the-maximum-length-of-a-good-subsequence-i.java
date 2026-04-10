2236800dd901e2b5d15cd880d0f5df7f89f6fead
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-10 19:21:46 +0530

    Time: 942 ms (22.22%) | Memory: 282.4 MB (10.00%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
@@ -1,35 +1,32 @@
 class Solution {
-    int[] nums;
-    int n;
-    int[][][] dp;
+    Integer[][][] dp;
     public int maximumLength(int[] nums, int k) {
-        this.nums = nums;
-        this.n = nums.length;
-        this.dp = new int[n][n + 1][k + 1];
-        for(int[][] mat: dp){
-            for(int[] arr : mat){
-                Arrays.fill(arr , -1);
-            }
-        }
-        return find(0 , -1 , k);
-    }
-    public int find(int ind ,int prev , int k){
-        if(ind >= n)return 0;
-        int prevKey = prev + 1;
-        if(dp[ind][prevKey][k] != -1)return dp[ind][prevKey][k];
+        // find max len of subseq
+        // at each step possiblity
+        // if no same do nothing
+        // if diff and k > 0 try reducing k
+        // start a new subseq
 
-        int skip = find(ind + 1, prev,k);
+        // now dp states n ,subseq size ,k
+        
+        int n = nums.length;
+        dp = new Integer[n][n + 1][k + 1];
+
+        return findMax(0,-1,nums , k);
+    }
+    public int findMax(int i,int prev , int[] nums , int k){
+        if(i == nums.length)return 0;
+        int p = prev + 1;
+        if(dp[i][p][k] != null)return dp[i][p][k];
         int take = 0;
-        // for take part
-        if(prev == -1){
-           take = 1 + find(ind + 1, ind , k);
+        int noTake = findMax(i + 1,prev , nums , k);
+        if(prev == -1 || nums[i] == nums[prev]){
+            take = 1 + findMax( i + 1 , i , nums, k);
         }else{
-            if(nums[prev] == nums[ind]){
-                take = 1 + find(ind + 1, ind , k);
-            }else if(k > 0){
-                take = 1 + find(ind + 1,ind , k - 1);
+            if(k > 0){
+                take = Math.max(take , 1 + findMax(i + 1 , i , nums , k - 1));
             }
         }
-        return dp[ind][prevKey][k] = Math.max(take , skip);
+        return dp[i][p][k] = Math.max(take , noTake);
     }
 }
