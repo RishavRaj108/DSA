b69f37ac71c46df77238d13900bb04156d255203
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-10 20:14:47 +0530

    Time: 1861 ms (5.56%) | Memory: 282.5 MB (8.89%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
@@ -19,13 +19,18 @@ class Solution {
         int p = prev + 1;
         if(dp[i][p][k] != null)return dp[i][p][k];
         int take = 0;
-        int noTake = findMax(i + 1,prev , nums , k);
-        if(prev == -1 || nums[i] == nums[prev]){
+        int noTake = 0;
+        if(prev == -1 ){
             take = 1 + findMax( i + 1 , i , nums, k);
+            noTake = findMax(i + 1,prev , nums , k);
         }else{
+            if(nums[i] == nums[prev]){
+                take = 1 + findMax( i + 1 , i , nums, k);
+            }
             if(k > 0){
                 take = Math.max(take , 1 + findMax(i + 1 , i , nums , k - 1));
             }
+            noTake = findMax(i + 1,prev , nums , k);
         }
         return dp[i][p][k] = Math.max(take , noTake);
     }
