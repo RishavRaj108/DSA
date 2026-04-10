d6bc4bc9345e729a2537d7457552a78af462b03e
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-10 21:01:47 +0530

    Time: 72 ms (54.84%) | Memory: 183.6 MB (60.48%) - LeetSync

--- 3363-most-frequent-ids/most-frequent-ids.java
+++ 3363-most-frequent-ids/most-frequent-ids.java
@@ -1,21 +1,27 @@
 class Solution {
     public long[] mostFrequentIDs(int[] nums, int[] freq) {
-        
-        int n = nums.length;
-        Map<Long,Long> mp = new HashMap<>();
+        // track freq of ids
+        // nums[i] rep id and freq[i] rep no of times that id should be added or removed
 
-        long[] res = new long[n];
-        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b) -> Long.compare(b[0] ,a[0]));
+        // at each index we need to find the cnt of most frequent id
+        // inorder to get maximum we use priority queue
+        int n = nums.length;
+        long[] ans = new long[n];
+        PriorityQueue<long[]> q = new PriorityQueue<>((a,b) -> Long.compare(b[0] , a[0]));
+
+        Map<Long , Long> mp = new HashMap<>();
+
         for(int i = 0;i < n;i++){
             long num = nums[i];
-            long f = freq[i];
-            mp.put(num , mp.getOrDefault(num , 0L) + f);
-            pq.add(new long[] {mp.get(num) , num});
-            while(!pq.isEmpty() && pq.peek()[0] != mp.get(pq.peek()[1])){
-                pq.poll();
+            long f = freq[i]; 
+            mp.put(num, mp.getOrDefault(num , 0L) + f);
+            q.add(new long[]{mp.get(num) , num});
+
+            while(!q.isEmpty() && mp.get(q.peek()[1]) != q.peek()[0]){
+                q.poll();
             }
-            res[i] = pq.peek()[0];
+            ans[i] = q.peek()[0];
         }
-        return res;
+        return ans;
     }
 }
