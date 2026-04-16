b5f202a241322668630347000aa74614f8cedb51
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-16 11:29:09 +0530

    Time: 84 ms (68.00%) | Memory: 134.9 MB (28.00%) - LeetSync

--- 3272-find-the-grid-of-region-average/find-the-grid-of-region-average.java
+++ 3272-find-the-grid-of-region-average/find-the-grid-of-region-average.java
@@ -1,52 +1,37 @@
 class Solution {
     public int[][] resultGrid(int[][] image, int threshold) {
-        int m = image.length, n = image[0].length;
-        // To store total of region averages for each cell
+        int m = image.length;
+        int n = image[0].length;
+
         int[][] sum = new int[m][n];
-        // To count how many regions include this cell
         int[][] count = new int[m][n];
 
-        // Traverse all possible 3x3 subgrids
-        for (int i = 0; i <= m - 3; i++) {
-            for (int j = 0; j <= n - 3; j++) {
-                // Step 1: Check if this 3x3 region is valid
+        for(int i = 0;i <= m - 3;i++){
+            for(int j = 0;j <= n - 3;j++){
                 boolean isValid = true;
-                // Check all adjacent pairs inside 3x3
-                for (int x = i; x < i + 3 && isValid; x++) {
-                    for (int y = j; y < j + 3; y++) {
-                        // Check right neighbor
-                        if (y + 1 < j + 3) {
-                            if (Math.abs(image[x][y] - image[x][y + 1]) > threshold) {
-                                isValid = false;
-                                break;
-                            }
+                for(int x = i;x < i + 3 && isValid;x++){
+                    for(int y = j;y < j + 3;y++){
+                        if(y + 1 < j + 3 && Math.abs(image[x][y] - image[x][y + 1]) > threshold){
+                           isValid = false;
+                           break;
                         }
-                        // Check down neighbor
-                        if (x + 1 < i + 3) {
-                            if (Math.abs(image[x][y] - image[x + 1][y]) > threshold) {
-                                isValid = false;
-                                break;
-                            }
+                        if(x + 1 < i + 3 && Math.abs(image[x][y] - image[x + 1][y]) > threshold){
+                            isValid = false;
+                            break;
                         }
                     }
                 }
-
-                // Step 2: If valid, compute region average
-                if (isValid) {
+                if(isValid){
                     int total = 0;
-
-                    // Sum all 9 cells
-                    for (int x = i; x < i + 3; x++) {
-                        for (int y = j; y < j + 3; y++) {
+                    for(int x = i;x < i + 3;x++){
+                        for(int y = j;y < j + 3;y++){
                             total += image[x][y];
                         }
                     }
+                    int avg = total / 9;
 
-                    int avg = total / 9; // floor automatically
-
-                    // Step 3: Add this avg to all 9 cells
-                    for (int x = i; x < i + 3; x++) {
-                        for (int y = j; y < j + 3; y++) {
+                    for(int x = i;x < i + 3;x++){
+                        for(int y = j;y < j + 3;y++){
                             sum[x][y] += avg;
                             count[x][y]++;
                         }
@@ -55,19 +40,17 @@ class Solution {
             }
         }
 
-        // Step 4: Build result grid
-        int[][] result = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // If belongs to at least one region
-                if (count[i][j] > 0) {
-                    result[i][j] = sum[i][j] / count[i][j];
-                } else {
-                    // Otherwise keep original
-                    result[i][j] = image[i][j];
+        int[][] res = new int[m][n];
+
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(count[i][j] > 0){
+                    res[i][j] = sum[i][j] / count[i][j];
+                }else{
+                    res[i][j] = image[i][j];
                 }
             }
         }
-        return result;
+        return res;
     }
 }
