a1a615b69212ba165b5fcb6795816c065f87b67b
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-04-17 10:20:15 +0530

    Time: 83 ms (72.00%) | Memory: 134.7 MB (38.00%) - LeetSync

--- 3272-find-the-grid-of-region-average/find-the-grid-of-region-average.java
+++ 3272-find-the-grid-of-region-average/find-the-grid-of-region-average.java
@@ -4,36 +4,37 @@ class Solution {
         int n = image[0].length;
 
         int[][] sum = new int[m][n];
-        int[][] count = new int[m][n];
+        int[][] cnt = new int[m][n];
 
-        for(int i = 0;i <= m - 3;i++){
-            for(int j = 0;j <= n - 3;j++){
-                boolean isValid = true;
-                for(int x = i;x < i + 3 && isValid;x++){
-                    for(int y = j;y < j + 3;y++){
-                        if(y + 1 < j + 3 && Math.abs(image[x][y] - image[x][y + 1]) > threshold){
-                           isValid = false;
-                           break;
+        for (int i = 0; i < m - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                boolean valid = true;
+
+                for (int x = i; x < i + 3 && valid; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        if (x + 1 < i + 3 && Math.abs(image[x][y] - image[x + 1][y]) > threshold) {
+                            valid = false;
+                            break;
                         }
-                        if(x + 1 < i + 3 && Math.abs(image[x][y] - image[x + 1][y]) > threshold){
-                            isValid = false;
+                        if (y + 1 < j + 3 && Math.abs(image[x][y] - image[x][y + 1]) > threshold) {
+                            valid = false;
                             break;
                         }
                     }
                 }
-                if(isValid){
+
+                if (valid) {
                     int total = 0;
-                    for(int x = i;x < i + 3;x++){
-                        for(int y = j;y < j + 3;y++){
+                    for (int x = i; x < i + 3; x++) {
+                        for (int y = j; y < j + 3; y++) {
                             total += image[x][y];
                         }
                     }
                     int avg = total / 9;
-
-                    for(int x = i;x < i + 3;x++){
-                        for(int y = j;y < j + 3;y++){
-                            sum[x][y] += avg;
-                            count[x][y]++;
+                    for (int x = i; x < i + 3; x++) {
+                        for (int y = j; y < j + 3; y++) {
+                             sum[x][y] += avg;
+                             cnt[x][y]++;
                         }
                     }
                 }
@@ -41,11 +42,10 @@ class Solution {
         }
 
         int[][] res = new int[m][n];
-
         for(int i = 0;i < m;i++){
             for(int j = 0;j < n;j++){
-                if(count[i][j] > 0){
-                    res[i][j] = sum[i][j] / count[i][j];
+                if(cnt[i][j] > 0){
+                    res[i][j] = sum[i][j] / cnt[i][j];
                 }else{
                     res[i][j] = image[i][j];
                 }
