21f4bb086ea0296adba2c9c0f7dc48275bceaff5
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-07 09:54:59 +0530

    Time: 21 ms (20.99%) | Memory: 47.1 MB (15.43%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,32 +1,29 @@
 class Solution {
     public int maximumJumps(int[] nums, int target) {
         int n = nums.length;
-        int[][] dp = new int[n][n];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
-        }
-        int val = find(0,1,target, nums,dp);
-        return val == Integer.MIN_VALUE/2 ? -1 : val;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        int val = find(0,target, dp,nums);
+        return val == Integer.MIN_VALUE? -1:val;
     }
-    public int find(int ind,int j , int target , int[] nums , int[][] dp){
-        if(ind == nums.length - 1){
-            return 0;
-        }
-        if(ind >= nums.length || j >= nums.length){
-            return Integer.MIN_VALUE/2;
-        }
-        if(dp[ind][j] != -1)return dp[ind][j];
+    public int find(int ind , int target , int[] dp , int[] nums){
+        if(ind == nums.length - 1)return 0;
 
+        if(ind >= nums.length)return Integer.MIN_VALUE;
 
-        int take = Integer.MIN_VALUE/2;
-        int notake = 0;
-        if(Math.abs(nums[ind] - nums[j]) <= target){
-            int val = find(j, j + 1,target , nums ,dp);
-            if(val != Integer.MIN_VALUE/2){
-                take = 1 + val;
+        if(dp[ind] != -1)return dp[ind];
+        int maxi = Integer.MIN_VALUE;
+        for(int i = ind + 1;i < nums.length;i++){
+            int val = Integer.MIN_VALUE;
+            if(Math.abs(nums[ind] - nums[i]) <= target){
+                val = find(i , target, dp,nums);
+                if(val != Integer.MIN_VALUE){
+                   val++;
+                }
             }
+            maxi = Math.max(maxi , val);
         }
-        int noTake = find(ind , j + 1,target,nums,dp);
-        return dp[ind][j] = Math.max(take ,noTake);
+        return dp[ind] = maxi;
     }
 }
