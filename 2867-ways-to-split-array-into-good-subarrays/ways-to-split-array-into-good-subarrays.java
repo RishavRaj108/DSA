7ee45fdd3a33f962ca135cffa70aba035ba566d4
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-08 18:30:42 +0530

    Time: 7 ms (92.79%) | Memory: 184.7 MB (40.54%) - LeetSync

--- 2867-ways-to-split-array-into-good-subarrays/ways-to-split-array-into-good-subarrays.java
+++ 2867-ways-to-split-array-into-good-subarrays/ways-to-split-array-into-good-subarrays.java
@@ -1,23 +1,21 @@
 class Solution {
     public int numberOfGoodSubarraySplits(int[] nums) {
-        long mod = 1000000007;
+        int n = nums.length;
+
+        int mod = 1000000007;
         long ans = 1;
-        int prev = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                // first 1
-                if (prev == -1) {
-                    prev = i;
-                }
-                else {
-                    // choices between consecutive 1s
-                    ans = (ans * (i - prev)) % mod;
-                    prev = i;
-                }
+        int prev= -1;
+        for(int i = 0;i < n;i++){
+           if(nums[i] == 1){
+            if(prev == -1){
+                prev = i;
+            }else{
+                ans = (ans * (i - prev)) % mod;
+                prev = i;
             }
+           }
         }
-        // no 1 present
-        if (prev == -1) return 0;
+        if(prev == -1)return 0;
         return (int) ans;
     }
 }
