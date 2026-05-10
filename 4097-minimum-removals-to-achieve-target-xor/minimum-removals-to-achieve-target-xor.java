92adf64a80d87efd5551acf584580f36eb0c1da9
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-10 17:57:02 +0530

    Time: 155 ms (75.22%) | Memory: 86.2 MB (27.41%) - LeetSync

--- 4097-minimum-removals-to-achieve-target-xor/minimum-removals-to-achieve-target-xor.java
+++ 4097-minimum-removals-to-achieve-target-xor/minimum-removals-to-achieve-target-xor.java
@@ -2,33 +2,31 @@ class Solution {
     public int minRemovals(int[] nums, int target) {
         int n = nums.length;
         // dp states index and elem & xor
-        int[][][] dp = new int[n][n][20050];
-        for(int[][] arr: dp){
-            for(int[] row : arr){
-                Arrays.fill(row , - 1);
-            }
+        int[][] dp = new int[n][20000];
+        for(int[] arr: dp){
+            Arrays.fill(arr, - 1);
         }
-        int val = find(0,0,0,nums,target,dp);
-        if(val == Integer.MIN_VALUE){
+        int val = find(0,0,nums,target,dp);
+        if(val < 0){
             return -1;
         }
         return n - val;
     }
-    public int find(int i,int ele,int xor,int[] nums , int target,int[][][] dp){
+    public int find(int i,int xor,int[] nums , int target,int[][] dp){
         if(i == nums.length){
           if(xor == target){
-            return ele;
+            return 0;
           }else{
-            return Integer.MIN_VALUE;
+            return -10000;
           }
         }
          
-        if(dp[i][ele][xor] != -1)return dp[i][ele][xor];
+        if(dp[i][xor] != -1)return dp[i][xor];
 
         // take 
-        int take = find(i + 1,ele + 1,xor ^ nums[i],nums,target,dp);
-        int noTake = find(i + 1,ele,xor,nums,target,dp);
+        int take = 1 + find(i + 1,xor ^ nums[i],nums,target,dp);
+        int noTake = find(i + 1,xor,nums,target,dp);
 
-        return dp[i][ele][xor]= Math.max(take , noTake);
+        return dp[i][xor]= Math.max(take , noTake);
     }
 }
