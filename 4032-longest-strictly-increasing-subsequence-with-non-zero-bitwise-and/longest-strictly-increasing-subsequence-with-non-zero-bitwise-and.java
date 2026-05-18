f20c8a1baba94af81c74a92dfb7987d4ec8f39c4
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-18 12:15:01 +0530

    Time: 1338 ms (8.33%) | Memory: 170.9 MB (16.67%) - LeetSync

--- 4032-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.java
+++ 4032-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.java
@@ -1,38 +1,32 @@
 class Solution {
     public int longestSubsequence(int[] nums) {
-        // bit wise and is not zero
-        // num & (1 << bit)
-        // generate list for each bit possible
-        // then try to generate lis using binary search technique
+        // lis
+        // with non zero bit wise and
 
+        // approch
+        // for all no the bit wise and should not be zero
+        // 
         int ans = 0;
-        for(int bit = 0;bit < 32;bit++){
+        for(int i = 0;i < 32;i++){
             List<Integer> vals = new ArrayList<>();
             for(int num : nums){
-                if((num & (1 << bit)) != 0){
-                  vals.add(num);
+                if((num & (1 << i)) != 0){
+                    vals.add(num);
                 }
             }
             if(vals.size() == 0)continue;
-
-            // now lis
-            List<Integer> list = new ArrayList<>();
-            for(int val : vals){
-
-                int pos = Collections.binarySearch(list, val);
-
-                if(pos < 0){
-                    // - pos - 1
-                    pos = - (pos + 1);
-                }
-
-                if(list.size()== pos){
-                    list.add(val);
+            List<Integer> ls = new ArrayList<>();
+            // now lis using binary search
+            for(int num : vals){
+                int pos = Collections.binarySearch(ls, num);
+                if(pos < 0)pos = -(pos + 1);
+                if(pos == ls.size()){
+                    ls.add(num);
                 }else{
-                    list.set(pos , val);
+                    ls.set(pos , num);
                 }
             }
-            ans = Math.max(list.size() , ans);
+            ans = Math.max(ans , ls.size());
         }
         return ans;
     }
