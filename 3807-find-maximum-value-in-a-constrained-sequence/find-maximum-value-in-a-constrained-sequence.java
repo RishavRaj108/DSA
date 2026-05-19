85efc249a9a4e2629fc312dcf6a1208095e87542
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-19 15:35:43 +0530

    Time: 7 ms (57.14%) | Memory: 207.9 MB (50.00%) - LeetSync

--- 3807-find-maximum-value-in-a-constrained-sequence/find-maximum-value-in-a-constrained-sequence.java
+++ 3807-find-maximum-value-in-a-constrained-sequence/find-maximum-value-in-a-constrained-sequence.java
@@ -1,25 +1,31 @@
 class Solution {
     public int findMaxVal(int n, int[][] restrictions, int[] diff) {
-        long arr[] = new long[n];
-        Arrays.fill(arr, Long.MAX_VALUE);
+        int[] arr = new int[n];
+        
+        Arrays.fill(arr, Integer.MAX_VALUE);
+
+        for(int[] vals : restrictions){
+            int ind = vals[0];
+            int maxV = vals[1];
+            arr[ind] = maxV;
+        }
         arr[0] = 0;
-        for(int ans[]: restrictions){
-            arr[ans[0]] = Math.min(arr[ans[0]], ans[1]);
+        for(int i = 0;i < n - 1;i++){
+            if(arr[i] != Integer.MAX_VALUE)
+           arr[i + 1] = Math.min(arr[i + 1] , arr[i] + diff[i]);
         }
-        for(int i=0; i<n-1; i++){
-            if(arr[i] != Long.MAX_VALUE){
-                arr[i+1] = Math.min(arr[i+1], arr[i] + diff[i]);
-            }        
+
+        for(int i = n - 2;i >= 0;i--){
+            if(arr[i + 1] != Integer.MAX_VALUE)
+            arr[i] = Math.min(arr[i] , arr[i + 1] + diff[i]);
         }
-        for(int i=n-2; i>=0; i--){
-            if(arr[i+1] != Long.MAX_VALUE){
-                arr[i] = Math.min(arr[i], arr[i+1] + diff[i]);
+
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            if(arr[i] > ans){
+                ans = arr[i];
             }
         }
-        long ans = 0;
-        for(long val: arr){
-            ans = Math.max(ans, val);
-        }
-        return (int)ans;
+        return ans;
     }
 }
