ae3e182196e0cf3780ef7d1ebdaeb96fb192a94d
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-22 11:50:35 +0530

    Time: 6 ms (87.04%) | Memory: 168.9 MB (37.96%) - LeetSync

--- 4077-lexicographically-smallest-negated-permutation-that-sums-to-target/lexicographically-smallest-negated-permutation-that-sums-to-target.java
+++ 4077-lexicographically-smallest-negated-permutation-that-sums-to-target/lexicographically-smallest-negated-permutation-that-sums-to-target.java
@@ -1,34 +1,42 @@
 class Solution {
     public int[] lexSmallestNegatedPerm(int n, long target) {
-        long total = (long)(n) * (n+1)/2;
-        long req = total - target;
+        // n 
+        // dictionary order smallest
+        // sum of elem == target
+        // abs permutation of n 1 2 3 ...  n
+        
+        // get sum then sub then elem by reduction 
+        // sum - target = redu 
+        // remove it from sum
 
-        if(req < 0 || req % 2 != 0){
-            return new int[]{};
+        long sum = (long)(n) * (n + 1) / 2;
+        sum = sum - target;
+        if(sum < 0 || sum % 2 != 0){
+            return new int[]{};// empty array
         }
-        long negativeSum = req/2;
-        boolean isNegative[] = new boolean[n+1];
-        for(int i=n; i>=1; i--){
-            if(negativeSum  >= i){
-                isNegative[i] = true;
-                negativeSum -= i;
+        sum = sum / 2;
+        boolean[] nega = new boolean[n + 1];
+
+        for(int i = n;i >= 1;i--){
+            if(sum >= i){
+                nega[i] = true;
+                sum -= i;
             }
         }
-        if(negativeSum != 0) return new int[]{};
-        int ans[] = new int[n];
-        int idx = 0;
-        for(int i=n; i>=1; i--){
-            if(isNegative[i]){
-                ans[idx] = -i;
-                idx++;
+        if(sum != 0)return new int[]{};
+        int[] res = new int[n];
+        int j = 0;
+        for(int i = n;i >= 0;i--){
+           if(nega[i]){
+            res[j++] = (-1 * i); 
+           }
+        }
+        for(int i = 1;i <= n;i++){
+            if(!nega[i]){
+                res[j++] = i;
             }
         }
-        for(int i=1; i<=n; i++){
-            if(!isNegative[i]){
-                ans[idx] = i;
-                idx++;
-            }
-        }
-        return ans;
+        return res;
+
     }
 }
