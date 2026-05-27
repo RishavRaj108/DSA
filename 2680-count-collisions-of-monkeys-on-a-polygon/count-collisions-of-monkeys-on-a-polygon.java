51b724b4024a87068c845b30e7f26fcd9d4a3e10
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-27 17:56:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.97%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.java
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.java
@@ -1,15 +1,16 @@
 class Solution {
     public int monkeyMove(int n) {
-        long mod = 1000000007;
         long ans = 1;
         long base = 2;
+        long mod = 1000000007;
+
         while(n > 0){
-          if(n % 2 == 1){
-            ans = (ans * base) % mod;
-          }
-          base = (base * base) % mod;
-          n = n / 2;
+            if(n % 2 == 1){
+                ans = (ans * base) % mod;
+            }
+            base = (base * base) % mod;
+            n = n/2;
         }
-        return (int)((ans - 2 + mod  ) % mod);
+        return (int)((ans + mod - 2) % mod) ;
     }
 }
