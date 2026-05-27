8f6d196e2aa3929b0ab2b68e653bc75b59e64c07
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-05-27 10:47:02 +0530

    Time: 86 ms (7.53%) | Memory: 85.8 MB (64.94%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,30 +1,28 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        // i have to find all pairs under this range
-
+        // i have to find all fair pairs
         int n = nums.length;
-        long cnt = 0;
         Arrays.sort(nums);
+        long cnt = 0;
         for(int i = 0;i < n;i++){
-           int num = nums[i];
-           int nl = lower - num;
-           int nu = upper - num + 1;
-           int left = lowerBound(nums , i + 1, n - 1, nl);
-           int right = lowerBound(nums , i + 1,n - 1, nu);
-           cnt += right - left;
+            int num = nums[i];
+            int nl = lower - num;
+            int nu = upper - num;
+            int left = lowerBound(nums , i + 1,n - 1,nl);
+            int right = lowerBound(nums , i + 1,n - 1,nu + 1);
+            cnt += right - left;
         }
         return cnt;
     }
-    public int lowerBound(int[] nums , int l,int r,int target){
-        while(l <= r){
-            int mid = l + ( r - l)/ 2;
-
+    public int lowerBound(int[] nums , int s,int e,int target){
+        while(s <= e){
+            int mid = s + (e - s)/2;
             if(nums[mid] >= target){
-                r = mid - 1;
+                e = mid - 1;
             }else{
-                l = mid + 1;
+                s = mid + 1;
             }
         }
-        return l;
+        return s;
     }
 }
