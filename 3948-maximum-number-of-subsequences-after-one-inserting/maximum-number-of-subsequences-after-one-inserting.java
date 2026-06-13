ff0c10cc193fd0b5e91e654da671408079c16519
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-06-13 09:51:19 +0530

    Time: 24 ms (71.85%) | Memory: 48 MB (32.59%) - LeetSync

---
+++ 3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.java
@@ -0,0 +1,52 @@
+class Solution {
+    public long numOfSubsequences(String s) {
+
+        long L = 0;
+        long LC = 0;
+        long LCT = 0;
+
+        long C = 0;
+        long CT = 0;
+
+        long totalT = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == 'T') totalT++;
+        }
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == 'L') {
+                L++;
+            }
+            else if(ch == 'C') {
+                LC += L;
+                C++;
+            }
+            else if(ch == 'T') {
+                LCT += LC;
+                CT += C;
+            }
+        }
+
+        long gainL = CT; // insert L at beginning
+        long gainT = LC; // insert T at end
+
+        long gainC = 0;
+
+        long leftL = 0;
+        long rightT = totalT;
+
+        for(char ch : s.toCharArray()) {
+
+            gainC = Math.max(gainC, leftL * rightT);
+
+            if(ch == 'L') leftL++;
+            if(ch == 'T') rightT--;
+        }
+
+        gainC = Math.max(gainC, leftL * rightT);
+
+        return LCT + Math.max(gainC, Math.max(gainL, gainT));
+    }
+}
