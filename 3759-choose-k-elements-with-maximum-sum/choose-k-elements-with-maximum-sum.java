5a2e517034f446f86b9f27baf905881b5e66cc18
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-06-22 21:10:08 +0530

    Time: 168 ms (50.43%) | Memory: 156.8 MB (59.13%) - LeetSync

--- 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.java
+++ 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.java
@@ -1,39 +1,31 @@
 class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
-        int[][] vec = new int[n][3]; //vec[i] = {nums1[i], i, nums2[i]}
-        for (int i = 0; i < n; i++) { //O(n)
-            vec[i][0] = nums1[i];
-            vec[i][1] = i;
-            vec[i][2] = nums2[i];
+        int[][] arr = new int[n][3];
+        for(int i = 0;i < n;i++){
+            arr[i][0] = i;
+            arr[i][1] = nums1[i];
+            arr[i][2] = nums2[i]; 
         }
 
-        Arrays.sort(vec , (a,b) -> a[0] - b[0]);
-        long[] ans = new long[n];
+        Arrays.sort(arr, (a,b) -> a[1] - b[1]);
+
+        long[] res = new long[n];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         long sum = 0;
-
         for(int i = 0;i < n;i++){
-            if(i > 0 && vec[i - 1][0] == vec[i][0]){
-                ans[vec[i][1]] = ans[vec[i - 1][1]];
+            if(i > 0 && arr[i][1] == arr[i - 1][1]){
+                res[arr[i][0]] = res[arr[i - 1][0]];
             }else{
-                ans[vec[i][1]] = sum;
+                res[arr[i][0]] = sum;
             }
-            pq.add(vec[i][2]);
-            sum += vec[i][2];
+
+            pq.add(arr[i][2]);
+            sum += arr[i][2];
             if(pq.size() > k){
                 sum -= pq.poll();
             }
         }
-        return ans;
+        return res;
     }
 }
-
-
-
-
-
-
-
-
-
