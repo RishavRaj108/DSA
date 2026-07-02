1c18ce5365736754c028e73d6966c6311e4d003a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-02 13:25:41 +0530

    Time: 23 ms (89.57%) | Memory: 81.2 MB (31.70%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,34 +1,30 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quan) {
-        int m = quan.length;
-        int low = 0;
-        int high = Integer.MIN_VALUE;
-        for(int num : quan){
-            high = Math.max(high , num);
-        }
-        int res = -1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(isPossible(mid,n , quan)){
-              res = mid;
-              high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
-        return res;
-    }
-    public boolean isPossible(int maxi,int n, int[] quan){
-        if(maxi == 0)return false;
-        boolean check = true;
-        
-        int cnt = 0;
-        for(int i = 0;i < quan.length;i++){
-            cnt += (quan[i] + maxi - 1)/maxi;
-            if(cnt > n)return false;
-        }
-        
+       int m = quan.length;
+       int low = 0;
+       int high = -1;
+       for(int num : quan){
+        high = Math.max(num , high);
+       }
 
-        return true;
+       int ans = 0;
+       while(low <= high){
+        int mid = low + (high - low)/2;
+        if(isPossible(mid , quan , n)){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+       }
+       return ans;
+    }
+    public boolean isPossible(int cap , int[] quan , int n){
+        if(cap == 0)return false;
+        int cnt = 0;
+        for(int num : quan){
+            cnt += (num + cap - 1)/cap;
+        }
+        return cnt <= n;
     }
 }
