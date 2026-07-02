3248369b0829615af694450a3b4d1162fc326f46
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-02 10:48:25 +0530

    Time: 126 ms (53.85%) | Memory: 179.9 MB (64.62%) - LeetSync

--- 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
+++ 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
@@ -1,69 +1,47 @@
-import java.util.*;
-
 class Solution {
-    public int maxCapacity(int[] costs, int[] capacity, int budget) {
+    public int maxCapacity(int[] costs, int[] capa, int budget) {
         int n = costs.length;
-        // Store cost and capacity together
-        // a[i][0] = cost
-        // a[i][1] = capacity
         int[][] a = new int[n][2];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i < n;i++){
             a[i][0] = costs[i];
-            a[i][1] = capacity[i];
+            a[i][1] = capa[i];
         }
-        // Sort by cost so binary search can be applied later
-        Arrays.sort(a, (x, y) -> x[0] - y[0]);
-
-        // best[i] = maximum capacity available from index [0...i]
-        // Helps us quickly get best possible partner item
+        Arrays.sort(a , (ab,b) -> ab[0] - b[0]);
         int[] best = new int[n];
         best[0] = a[0][1];
-        for (int i = 1; i < n; i++) {
-            best[i] = Math.max(best[i - 1], a[i][1]);
+        for(int i = 1;i< n;i++){
+          best[i] = Math.max(best[i - 1] , a[i][1]);
         }
-
         int ans = 0;
-        // ---------------------------------------------------
-        // Case 1: Choose only one item
-        // ---------------------------------------------------
-        for (int i = 0; i < n; i++) {
-            // Cost must be strictly less than budget
-            if (a[i][0] < budget) {
-                ans = Math.max(ans, a[i][1]);
+        // choose only one
+        for(int i = 0;i < n;i++){
+            if(a[i][0] < budget){
+                ans = Math.max(ans , a[i][1]);
             }
         }
-        // ---------------------------------------------------
-        // Case 2: Choose two items
-        // ---------------------------------------------------
-        for (int j = 1; j < n; j++) {
-            // Remaining money allowed for first item
-            // total cost must stay < budget
-            int rem = budget - a[j][0] - 1;
-            // Find rightmost item having cost <= rem
-            int i = upperBound(a, rem, j - 1);
-            // If valid partner exists
-            if (i >= 0) {
-                // best[i] gives maximum capacity till index i
-                ans = Math.max(ans, best[i] + a[j][1]);
-            }
+        // choose two value
+        for(int i = 1;i < n;i++){
+            // choose this index 
+            int rem = budget - a[i][0] - 1;
+            int ind = upperBound(rem , a , i - 1);
+            if(ind < 0)continue;
+            ans = Math.max(ans , best[ind] + a[i][1]);
         }
-        return ans;
+        return ans ;
     }
-
-    // Binary search:
-    // Returns rightmost index having cost <= val
-    private int upperBound(int[][] a, int val, int hi) {
-        int l = 0, r = hi;
-        int ans = -1;
-        while (l <= r) {
-            int m = (l + r)/2;
-            if (a[m][0] <= val) {
-                ans = m;
-                l = m + 1; // try finding bigger valid index
-            } else {
-                r = m - 1;
-            }
+    public int upperBound(int cap , int[][] a , int hi){
+       int res = -1;
+       int l = 0;
+       int r = hi;
+       while(l <= r){
+        int mid = l + (r - l)/ 2;
+        if(a[mid][0] <= cap){
+            res = mid;
+            l = mid + 1;// try larger value;
+        }else{
+            r = mid - 1;
         }
-        return ans;
+       }
+       return res;
     }
 }
