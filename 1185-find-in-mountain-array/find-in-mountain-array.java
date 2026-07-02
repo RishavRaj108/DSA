6401e9721d8aa210f98d81e3f93ef371993610b7
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-02 09:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (81.00%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.java
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -10,10 +10,6 @@
 class Solution {
     public int findInMountainArray(int target, MountainArray mouArr) {
         int n = mouArr.length();
-
-        // int low = 0;
-        // int high = n - 1;
-        // int peak = 0;
         int low = 0, high = n - 1;
 
         while (low < high) {
@@ -27,28 +23,6 @@ class Solution {
         }
 
         int peak = low;
-        // while (low < high) {
-        //     int mid = low + (high - low) / 2;
-        //      if(high - low + 1 < 3 && (mid == 0 || mid == n - 1)){
-        //         if(mid == 0){
-        //             peak = 1;
-        //         }else{
-        //             peak = n - 2;
-        //         }
-        //         break;
-        //      }
-        //      int prev = mouArr.get(mid - 1);
-        //      int curr = mouArr.get(mid);
-        //      int next = mouArr.get(mid + 1);
-        //      if(prev < curr && curr < next)low = mid + 1;
-        //      else if(curr < prev && curr > next)high = mid - 1;
-        //      else if(curr > prev && curr > next){
-        //         peak = mid;
-        //         break;
-        //      }
-        // }
-        //    return peak;
-        // now i need to earch in both the space
         low = 0;
         high = peak;
         while (low <= high) {
