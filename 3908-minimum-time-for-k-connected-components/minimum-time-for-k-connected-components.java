04f2c5ddbdf63ea83b8eefd314561651e4b20404
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-03 10:28:02 +0530

    Time: 391 ms (11.00%) | Memory: 279.3 MB (11.00%) - LeetSync

--- 3908-minimum-time-for-k-connected-components/minimum-time-for-k-connected-components.java
+++ 3908-minimum-time-for-k-connected-components/minimum-time-for-k-connected-components.java
@@ -1,53 +1,48 @@
 class Solution {
     public int minTime(int n, int[][] edges, int k) {
-        // build graph
         List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0;i < n;i++){
-            adj.add(new ArrayList<>());
-        }
-        int maxT = 0;
+        for(int i = 0;i < n;i++)adj.add(new ArrayList<>());
+        int low = 0;
+        int high = Integer.MIN_VALUE;
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
-            int time = edge[2];
-            adj.get(u).add(new int[]{v,time});
-            adj.get(v).add(new int[]{u,time});
-            maxT = Math.max(maxT , time);
+            int t = edge[2];
+            high = Math.max(high , t);
+            adj.get(u).add(new int[]{v, t});
+            adj.get(v).add(new int[]{u, t});
         }
-
-        int start = 0;
-        int end = maxT;
-        while(start <= end){
-            int mid = start + (end - start)/ 2;
-
-            if(isPossible(adj,mid, k)){
-                end = mid - 1;
+        int res = 0;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isPossible(adj , mid , k)){
+                res = mid;
+                high = mid - 1;
             }else{
-                start = mid + 1;
+                low = mid + 1;
             }
         }
-        return start;
+        return res;
     }
-    public boolean isPossible(List<List<int[]>> adj,int t,int k){
+    public boolean isPossible(List<List<int[]>> adj,int time , int comp){
         int cnt = 0;
         int n = adj.size();
         boolean[] vis = new boolean[n];
-        for(int i = 0;i < adj.size();i++){
+        for(int i = 0;i < n;i++){
             if(!vis[i]){
                 cnt++;
-                dfs(i,adj,t,vis);
+                dfs(adj,time,i,vis);
             }
         }
-        return cnt >= k;
+        return cnt >= comp;
     }
-    public void dfs(int node , List<List<int[]>> adj,int t,boolean[] vis){
+    public void dfs(List<List<int[]>> adj,int time , int node,boolean[] vis){
         vis[node] = true;
-
-        for(int[] neg : adj.get(node)){
-            int n = neg[0];
-            int time = neg[1];
-            if(!vis[n] && time > t){
-                dfs(n , adj , t,vis);
+        for(int[] neig : adj.get(node)){
+            int ng = neig[0];
+            int t = neig[1];
+            if(!vis[ng] && t > time){
+                dfs(adj,time,ng,vis);
             }
         }
     }
