be96dfd9581643c3c757769969949a1553cf8e7c
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-07 12:07:28 +0530

    Time: 36 ms (36.36%) | Memory: 47.5 MB (53.64%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
@@ -1,19 +1,26 @@
 class Solution {
     public long calculateScore(String s) {
         long score = 0;
-        ArrayDeque<Integer>[] mp = new ArrayDeque[26];
-        for (int i = 0; i < 26; i++) {
-            mp[i] = new ArrayDeque<>();
-        }
-        for (int i = 0; i < s.length(); i++) {
-            int ch = s.charAt(i) - 'a';
-            int mirror = 25 - ch;
-            if (!mp[mirror].isEmpty()) {
-                int j = mp[mirror].pollLast();
-                score += i - j;
-            } else {
-                mp[ch].addLast(i);
+        String first = "abcdefghijklmnopqrstuvwxyz";
+        String second ="zyxwvutsrqponmlkjihgfedcba";
+
+        Map<Character, List<Integer>> mp = new HashMap<>();
+        int n = s.length();
+        
+        for(int i = 0;i < n;i++){
+            char ch = s.charAt(i);
+            int ind = ch - 'a';
+            char mirror = second.charAt(ind);
+            if(mp.containsKey(mirror)){
+               List<Integer> ls = mp.get(mirror);
+               score += i - ls.get(ls.size() - 1);
+               ls.remove(ls.size() - 1);
+               if(ls.size() == 0){
+                mp.remove(mirror);
+               }
+               continue;
             }
+            mp.computeIfAbsent(ch ,k -> new ArrayList<>()).add(i);
         }
         return score;
     }
