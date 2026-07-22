b12bcdb41ba0dee45995f5c3eb4ccbc4a48f67f9
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-22 16:02:33 +0530

    Time: 40 ms (70.14%) | Memory: 67.1 MB (85.42%) - LeetSync

--- 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.java
+++ 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.java
@@ -1,23 +1,25 @@
 class Solution {
     public long countPairs(String[] words) {
-        int n = words.length;
-        Map<String , Long> mp = new HashMap<>();
-        long ans = 0;
-        for(String word : words){
-            String str = build(word);
-            long cnt = mp.getOrDefault(str, 0L);
-            ans += cnt;
-            mp.put(str, cnt + 1);
+        Map<String, Integer> gapMap = new HashMap<>();
+        for (String word : words) {
+            String gaps = gap(word);
+            gapMap.put(gaps, gapMap.getOrDefault(gaps, 0) + 1);
         }
-        return ans;
+        long pairs = 0;
+        for (int count : gapMap.values()) {
+            pairs += (long) count * (count - 1) / 2;
+        }
+        return pairs;
     }
-    public String build(String word){
-        StringBuilder sb = new StringBuilder();
-        char base = word.charAt(0);
-        for(int i = 0;i < word.length();i++){
-            int val = (word.charAt(i) - base + 26)% 26;
-            sb.append(val).append('#');  
+    private String gap(String word) {
+        int n = word.length();
+        char[] gaps = new char[n - 1];
+        for (int i = 1; i < n; i++) {
+            int curr = word.charAt(i);
+            int prev = word.charAt(i - 1);
+            int diff = (curr - prev + 26) % 26;
+            gaps[i - 1] = (char) (diff + 'a');
         }
-        return sb.toString();
+        return new String(gaps);
     }
 }
