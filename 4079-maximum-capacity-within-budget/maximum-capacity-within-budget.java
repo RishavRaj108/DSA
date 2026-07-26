332f4938e4af6fb4ad2301de93b12b1b87ed0300
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-26 10:04:56 +0530

    Time: 122 ms (65.99%) | Memory: 180 MB (51.70%) - LeetSync

--- 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
+++ 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
@@ -1,47 +1,50 @@
 class Solution {
-    public int maxCapacity(int[] costs, int[] capa, int budget) {
+    public int maxCapacity(int[] costs, int[] capacity, int budget) {
         int n = costs.length;
         int[][] a = new int[n][2];
         for(int i = 0;i < n;i++){
             a[i][0] = costs[i];
-            a[i][1] = capa[i];
+            a[i][1] = capacity[i];
         }
-        Arrays.sort(a , (ab,b) -> ab[0] - b[0]);
+        budget--;
+        Arrays.sort(a , (x,y) -> x[0] - y[0]);
         int[] best = new int[n];
         best[0] = a[0][1];
-        for(int i = 1;i< n;i++){
-          best[i] = Math.max(best[i - 1] , a[i][1]);
+        for(int i = 1;i < n;i++){
+           best[i] = Math.max(best[i - 1] , a[i][1]);
         }
+
         int ans = 0;
-        // choose only one
+        // choose one 
         for(int i = 0;i < n;i++){
-            if(a[i][0] < budget){
-                ans = Math.max(ans , a[i][1]);
+           if(a[i][0] <= budget){
+            ans = best[i];
+           }else{
+            break;
+           }
+        }
+        for(int i = 1;i < n;i++){
+            int rem = budget - a[i][0];
+            if(rem < 0)break;
+            int second = find(a , rem , i - 1);
+            if(second == -1)continue;
+            ans = Math.max(ans , a[i][1] + best[second]);
+        }
+        return ans;
+    }
+    public int find(int[][] a , int cost , int h){
+        int l = 0;
+        int res = -1;
+
+        while(l <= h){
+            int mid = l + (h - l)/ 2;
+            if(a[mid][0] > cost){
+                h = mid - 1;
+            }else{
+                res = mid;
+                l = mid + 1;
             }
         }
-        // choose two value
-        for(int i = 1;i < n;i++){
-            // choose this index 
-            int rem = budget - a[i][0] - 1;
-            int ind = upperBound(rem , a , i - 1);
-            if(ind < 0)continue;
-            ans = Math.max(ans , best[ind] + a[i][1]);
-        }
-        return ans ;
-    }
-    public int upperBound(int cap , int[][] a , int hi){
-       int res = -1;
-       int l = 0;
-       int r = hi;
-       while(l <= r){
-        int mid = l + (r - l)/ 2;
-        if(a[mid][0] <= cap){
-            res = mid;
-            l = mid + 1;// try larger value;
-        }else{
-            r = mid - 1;
-        }
-       }
-       return res;
+        return res;
     }
 }
