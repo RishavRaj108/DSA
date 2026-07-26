74aec4f4b1ef7aac4446f6c4e8c501f48811ee0b
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-07-26 16:00:48 +0530

    Time: 30 ms (21.20%) | Memory: 46.6 MB (94.91%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -1,47 +1,58 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        int hel;
+        Pair(int row , int col , int hel){
+            this.row = row;
+            this.col = col;
+            this.hel = hel;
+        }
+    }
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
-        // just go for bfs approch
         int m = grid.size();
         int n = grid.get(0).size();
-
         int[][] best = new int[m][n];
-        for(int[] row : best){
-            Arrays.fill(row , - 1);
+        for(int[] arr : best){
+            Arrays.fill(arr , -1);
+        }
+        Queue<Pair> q = new LinkedList<>();
+        if(grid.get(0).get(0) == 1)health--;
+        best[0][0] = health;
+        q.add(new Pair(0,0,health));
+        int[][] dirs = {{-1,0},{0,-1},{0,1},{1,0}};
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int r = p.row;
+            int c = p.col;
+            int hel = p.hel;
+
+            for(int[] dir : dirs){
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+                int val = hel;
+                if(nr >= m || nr < 0 || nc >= n || nc < 0)continue;
+                if(grid.get(nr).get(nc) == 1)val--;
+                if(best[nr][nc] >= val)continue;
+                best[nr][nc] = val;
+                if(val > 0){
+                    if(nr == m - 1 && nc == n - 1)return true;
+                    q.add(new Pair(nr,nc , val));
+                }
+            }
         }
 
-        Queue<int[]> q = new LinkedList<>();
-        int start = health - grid.get(0).get(0);
-        if(start <= 0) return false;
-
-        q.offer(new int[]{0,0,start});
-
-        best[0][0] = start;
-
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-            int h = cur[2];
-
-            if(r == m - 1 && c == n - 1)return true;
-
-            for(int[] d: dirs){
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if(nr >= m || nc >= n || nr < 0 || nc < 0)continue;
-
-                int newHeal = h - grid.get(nr).get(nc);
-                if(newHeal <= 0)continue;
-
-                if(best[nr][nc] >= newHeal)continue;
-
-                best[nr][nc] = newHeal;
-                q.offer(new int[]{nr,nc ,newHeal});
-            }
-        } 
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
