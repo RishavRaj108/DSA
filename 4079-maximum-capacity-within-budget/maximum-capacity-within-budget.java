d9c32ec356f15ce833f54210e3204b163a819457
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-05 16:53:08 +0530

    Time: 124 ms (60.40%) | Memory: 179.6 MB (74.50%) - LeetSync

--- 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
+++ 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
@@ -1,50 +1,48 @@
 class Solution {
-    public int maxCapacity(int[] costs, int[] capacity, int budget) {
+    public int maxCapacity(int[] costs, int[] capa, int budget) {
         int n = costs.length;
-        int[][] a = new int[n][2];
-        for(int i = 0;i < n;i++){
-            a[i][0] = costs[i];
-            a[i][1] = capacity[i];
-        }
         budget--;
-        Arrays.sort(a , (x,y) -> x[0] - y[0]);
-        int[] best = new int[n];
-        best[0] = a[0][1];
-        for(int i = 1;i < n;i++){
-           best[i] = Math.max(best[i - 1] , a[i][1]);
-        }
-
-        int ans = 0;
-        // choose one 
+        int[][] vals = new int[n][2];
         for(int i = 0;i < n;i++){
-           if(a[i][0] <= budget){
-            ans = best[i];
-           }else{
-            break;
-           }
+            vals[i][0] = costs[i];
+            vals[i][1] = capa[i];
         }
-        for(int i = 1;i < n;i++){
-            int rem = budget - a[i][0];
-            if(rem < 0)break;
-            int second = find(a , rem , i - 1);
-            if(second == -1)continue;
-            ans = Math.max(ans , a[i][1] + best[second]);
-        }
-        return ans;
-    }
-    public int find(int[][] a , int cost , int h){
-        int l = 0;
-        int res = -1;
+        Arrays.sort(vals , (a,b) -> a[0] - b[0]);
 
+        int[] best = new int[n];
+        best[0] = vals[0][1];
+        for(int i = 1;i < n;i++){
+            int curr = vals[i][1];
+            best[i] = Math.max(best[i - 1] , vals[i][1]);
+        }
+        int maxi = 0;
+        // choose one
+        for(int i = 0;i < n;i++){
+            if(vals[i][0] <= budget)
+            maxi = Math.max(maxi , vals[i][1]);
+        }
+        // choose two 
+        for(int i = 0;i < n;i++){
+            int rem = budget - vals[i][0];
+            if(rem < 0)break;
+            int ind = find(vals, rem , i - 1);
+            if(ind == -1)continue;
+            maxi = Math.max(maxi , best[ind] + vals[i][1]);
+        }
+        return maxi;
+    }
+    public int find(int[][] vals , int capa , int h){
+        int l = 0;
+        int ans = -1;
         while(l <= h){
-            int mid = l + (h - l)/ 2;
-            if(a[mid][0] > cost){
-                h = mid - 1;
-            }else{
-                res = mid;
+            int mid = l + (h - l)/2;
+            if(vals[mid][0] <= capa){
+                ans = mid;
                 l = mid + 1;
+            }else{
+                h = mid - 1;
             }
         }
-        return res;
+        return ans;
     }
 }
