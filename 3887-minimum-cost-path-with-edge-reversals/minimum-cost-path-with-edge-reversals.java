4e88dcc9754a532ec03ff5a4229b62a98ad27a44
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-07 14:00:35 +0530

    Time: 76 ms (76.04%) | Memory: 265.5 MB (58.13%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,39 +1,44 @@
 class Solution {
     public int minCost(int n, int[][] edges) {
         List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0;i < n;i++)adj.add(new ArrayList<>());
+        for(int i = 0;i < n;i++){
+            adj.add(new ArrayList<>());
+        }
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
-            int w = edge[2];
-            adj.get(u).add(new int[]{v,w});
-            adj.get(v).add(new int[]{u, 2 * w});
+            int wt = edge[2];
+            adj.get(u).add(new int[]{v , wt});
+            adj.get(v).add(new int[]{u , 2 * wt});
         }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-
         int[] dist = new int[n];
         Arrays.fill(dist , Integer.MAX_VALUE);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
 
         pq.add(new int[]{0,0});
         dist[0] = 0;
         while(!pq.isEmpty()){
             int[] vals = pq.poll();
-            int node = vals[0];
-            int w = vals[1];
-            if(dist[node] < w)continue;
-            if(node == n - 1)return w;
+            int u = vals[0];
+            int wht = vals[1];
+            if(u == n -1)return wht;
 
-            for(int[] negi : adj.get(node)){
-                int nnode = negi[0];
-                int wt = negi[1];
-                int tw = wt + w;
-                if(dist[nnode] > tw){
-                    pq.add(new int[]{nnode , tw});
-                    dist[nnode] = tw;
+            for(int[] neighbors : adj.get(u)){
+                int v = neighbors[0];
+                int wt = neighbors[1];
+                if(wt + wht < dist[v]){
+                    
+                    dist[v] = wt + wht;
+                    pq.add(new int[]{v , wt + wht});
                 }
             }
         }
         return -1;
     }
 }
+
+
+
+
+
