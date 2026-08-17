1433f4b51329ce127e1aac3dd0cdefe09bc9366b
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-17 13:19:15 +0530

    Time: 90 ms (47.10%) | Memory: 100.6 MB (37.63%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,53 +1,49 @@
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        // dag acyclic
+        // so topo
+        // all ancistors done
+        // add the current and all the ansistors of the current node
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i= 0;i < n;i++){
-            adj.add(new ArrayList<>());
-        }
-        int[] indegree = new int[n];
-        for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            indegree[v]++;
-        }
-
-        List<Set<Integer>> anses = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-
+        List<Set<Integer>> res = new ArrayList<>();
         for(int i = 0;i < n;i++){
-            anses.add(new HashSet<>());
-            if(indegree[i] == 0)q.add(i);
+            adj.add(new ArrayList<>());
+            res.add(new HashSet<>());
         }
-        while(!q.isEmpty()){
-           int node = q.poll();
-
-           for(int neig : adj.get(node)){
-            anses.get(neig).add(node);
-            anses.get(neig).addAll(anses.get(node));
-
-            indegree[neig]--;
-            if(indegree[neig] == 0){
-                q.add(neig);
+        int[]indegree = new int[n];
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            indegree[edge[1]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0;i < n;i++){
+            if(indegree[i] == 0){
+                q.add(i);
             }
-           }
+        }
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int neig : adj.get(node)){
+                
+                res.get(neig).addAll(res.get(node));
+                    res.get(neig).add(node);
+
+                indegree[neig]--;
+                if(indegree[neig] == 0){
+                    
+                    q.add(neig);
+                }
+            }
+        }
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(Set<Integer> ls : res){
+            result.add(new ArrayList<>(ls));
+            Collections.sort(result.get(result.size() - 1));
         }
 
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0;i< n;i++){
-            res.add(new ArrayList<>(anses.get(i)));
-            Collections.sort(res.get(i));
-        }
-        return res;
+        return result;
     }
 }
-
-
-
-
-
-
-
-
-
-
