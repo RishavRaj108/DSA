4d0b4f0f26da164bb6ada8abbb9868d3e8f2b456
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-18 09:58:50 +0530

    Time: 124 ms (25.97%) | Memory: 278.5 MB (87.86%) - LeetSync

--- 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.java
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.java
@@ -1,41 +1,44 @@
 class Solution {
-    static final int MOD = 1_000_000_007;
     public int assignEdgeWeights(int[][] edges) {
-        
         List<List<Integer>> adj = new ArrayList<>();
         int n = edges.length + 1;
-        for(int i = 0;i <= n;i++){
-            adj.add(new ArrayList<>());
-        }
+
+        for(int i = 0;i <= n;i++)adj.add(new ArrayList<>());
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
+
         boolean[] vis = new boolean[n + 1];
-        vis[1] = true;
+        Queue<Integer> q = new LinkedList<>();
+         vis[1] = true;
+        q.add(1);
+        
+
         int len = 0;
         while(!q.isEmpty()){
-           int size = q.size();
-           len++;
-           for(int i = 0;i < size;i++){
-            int node = q.poll();
-            vis[node] = true;
-            for(int ng : adj.get(node)){
-                if(!vis[ng]){
-                    q.add(ng);
+            int size = q.size();
+            len++;
+            for(int i = 0; i < size;i++){
+                int node = q.poll();
+              
+               for(int neig : adj.get(node)){
+                if(!vis[neig]){
+                    vis[neig] = true;
+                    q.add(neig);
                 }
+               }
             }
-           }
         }
-        len = len - 1;
+        len--;
+        int mod = 1000000007;
         long ways = 1;
-        for(int i = 1;i < len;i += 1){
-           ways = (ways * 2) % MOD;
+        for(int i = 1;i < len;i++){
+            ways = (1L * ways * 2) % mod;
         }
-        return (int) ways;
+        return (int)ways;
+
     }
 }
