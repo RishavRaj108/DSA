d6a27a97d8e7318753d8a3fac26c0c6e5524f50a
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-20 10:34:53 +0530

    Time: 13 ms (26.39%) | Memory: 155.2 MB (43.47%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -0,0 +1,33 @@
+class Solution {
+    long[] dp;
+
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        dp = new long[n];
+
+        Arrays.fill(dp, -1);
+
+        return find(0, questions);
+    }
+
+    public long find(int i, int[][] questions) {
+        int n = questions.length;
+
+        if (i >= n) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        // Skip question i
+        long skip = find(i + 1, questions);
+
+        // Solve question i
+        int next = i + questions[i][1] + 1;
+        long solve = questions[i][0] + find(next, questions);
+
+        return dp[i] = Math.max(skip, solve);
+    }
+}
