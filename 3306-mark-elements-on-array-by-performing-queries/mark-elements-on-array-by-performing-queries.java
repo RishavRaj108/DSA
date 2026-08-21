fa6a5388dca2e167c1f34a7b2377ca44660fb5c3
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-21 13:55:05 +0530

    Time: 148 ms (52.31%) | Memory: 158.6 MB (90.00%) - LeetSync

--- 3306-mark-elements-on-array-by-performing-queries/mark-elements-on-array-by-performing-queries.java
+++ 3306-mark-elements-on-array-by-performing-queries/mark-elements-on-array-by-performing-queries.java
@@ -1,46 +1,42 @@
 class Solution {
-    public long[] unmarkedSumArray(int[] nums, int[][] queries) {
+    public long[] unmarkedSumArray(int[] nums, int[][] quer) {
         int n = nums.length;
-        int m = queries.length;
-
-        int[] marked = new int[n];
-        long total = 0;
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) ->{
-        if(a[0] != b[0]){
-            return a[0] - b[0];
-        }return a[1] - b[1];
-        } );
-
-        // int[] formate num , ind
+        int m = quer.length;
+        boolean[] marked = new boolean[n];
+        long sum = 0L;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+            if(a[0] != b[0])return a[0] - b[0];
+            else return a[1] - b[1]; 
+        });
 
         for(int i = 0;i < n;i++){
             int num = nums[i];
-            total += num;
-            q.add(new int[]{num , i});
+            sum += num;
+            pq.add(new int[]{num , i});
         }
-        long prefix = 0;
+
         long[] res = new long[m];
         int i = 0;
-        for(int[] quer : queries){
-            int ind = quer[0];
-            int k = quer[1];
-            if(marked[ind] == 0){
-                marked[ind] = 1;
-            prefix += nums[ind];
+        for(int[] q : quer){
+            int ind = q[0];
+            int k = q[1];
+            if(!marked[ind]){
+                sum -= nums[ind];
+                marked[ind] = true;
             }
-            while(!q.isEmpty() && k > 0){
-               int[] vals = q.poll();
-               int num = vals[0];
-               ind = vals[1];
-               if(marked[ind] == 0){
-                k--;
-                prefix += num;
-                marked[ind] = 1;
-               }
-               if(prefix == total)break;
+
+            while(k-- > 0){
+                while(!pq.isEmpty() && marked[pq.peek()[1]] == true){
+                   pq.poll();
+                }
+                if(pq.isEmpty()){
+                    break;
+                }
+                int[] vals = pq.poll();
+                sum -= vals[0];
+                marked[vals[1]] = true;
             }
-            if(prefix == total)break;
-            res[i++] = total - prefix;
+            res[i++] = sum; 
         }
         return res;
     }
