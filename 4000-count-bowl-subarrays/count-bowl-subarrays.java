f4dfbaee9cb19f6ceca9db66795d84db39567270
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-21 19:35:10 +0530

    Time: 166 ms (7.25%) | Memory: 134.9 MB (9.33%) - LeetSync

--- 4000-count-bowl-subarrays/count-bowl-subarrays.java
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.java
@@ -1,29 +1,28 @@
 class Solution {
     public long bowlSubarrays(int[] nums) {
+        long cnt = 0L;
         int n = nums.length;
-
-        int[] left = new int[n];
+        int[]left = new int[n];
         int[] right = new int[n];
 
         Arrays.fill(left , -1);
         Arrays.fill(right , n);
         Stack<Integer> st = new Stack<>();
-
         for(int i = 0;i < n;i++){
-            int num = nums[i];
-            while(!st.isEmpty() && nums[st.peek()] < num){
-                st.pop();
-            }
-
-            if(!st.isEmpty()){
-                left[i] = st.peek();
-            }
-            st.push(i);
+           int num = nums[i];
+           while(!st.isEmpty() && nums[st.peek()] < num){
+            st.pop();
+           }
+           if(!st.isEmpty()){
+            left[i] = st.peek();
+           }
+           st.push(i);
         }
         st.clear();
 
         for(int i = n - 1;i >= 0;i--){
-            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+            int num = nums[i];
+            while(!st.isEmpty() && nums[st.peek()] < num){
                 st.pop();
             }
             if(!st.isEmpty()){
@@ -32,14 +31,13 @@ class Solution {
             st.push(i);
         }
 
-        long cnt = 0;
         for(int i = 0;i < n;i++){
             if(left[i] != -1 && i - left[i] >= 2){
                 cnt++;
             }
             if(right[i] != n && right[i] - i >= 2){
                 cnt++;
-            }
+            } 
         }
         return cnt;
     }
