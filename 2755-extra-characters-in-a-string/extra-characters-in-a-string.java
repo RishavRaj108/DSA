5f5345d15f991a752bb484e9a1d24bbe8a545bed
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-25 10:11:11 +0530

    Time: 47 ms (50.56%) | Memory: 47.1 MB (23.02%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,29 +1,29 @@
 class Solution {
-    int[]t;
+    int[] dp;
     public int minExtraChar(String s, String[] dict) {
         int n = s.length();
-        t = new int[n];
-        Arrays.fill(t , -1);
+        dp = new int[n];
+        Arrays.fill(dp , -1);
         Set<String> set = new HashSet<>();
         for(String str : dict){
-            set.add(str);
+           set.add(str);
         }
-        return find(0,set,s);
+        return find(0,s,set);
     }
-    public int find(int i , Set<String> set,String s){
+    public int find(int i,String s,Set<String> set){
+        int n = s.length();
         if(i >= s.length())return 0;
-        if(t[i] != -1)return t[i];
+        if(dp[i] != -1)return dp[i];
 
-        int skip = 1 + find(i + 1,set,s);
-        int take = Integer.MAX_VALUE;
+        int extra = 1 + find(i + 1,s,set);
 
-        for(int j = i;j < s.length();j++){
-            String sub = s.substring(i , j + 1);
-
-            if(set.contains(sub)){
-                take = Math.min(take , find(j + 1,set,s));
+        int poss = Integer.MAX_VALUE/2;
+        for(int j = i;j < n;j++){
+            String str = s.substring(i , j + 1);
+            if(set.contains(str)){
+                poss = Math.min(poss , find(j + 1 , s,set));
             }
         }
-        return t[i] =  Math.min(take , skip);
+        return dp[i] = Math.min(extra , poss);
     }
 }
