922b70a82b10c0201abde6c6c1400df17f2425da
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-08-25 09:50:57 +0530

    Time: 162 ms (64.83%) | Memory: 57.6 MB (16.55%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
@@ -1,34 +1,29 @@
-import java.util.*;
-
 class Solution {
     public String clearStars(String s) {
         int n = s.length();
         PriorityQueue<Character> pq = new PriorityQueue<>();
-        Map<Character, Deque<Integer>> map = new HashMap<>();
-        boolean[] keep = new boolean[n];
-        Arrays.fill(keep, true);
+        Map<Character , Deque<Integer>> mp = new HashMap<>();
+        boolean[] check = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (c == '*') {
-                char smallest = pq.poll();
-                int indexToRemove = map.get(smallest).removeLast();
-                keep[i] = false;               // Remove '*'
-                keep[indexToRemove] = false;   // Remove smallest char
-            } else {
-                pq.offer(c);
-                map.putIfAbsent(c, new ArrayDeque<>());
-                map.get(c).add(i);
+        for(int i = 0;i < n;i++){
+            char ch = s.charAt(i);
+            if(ch == '*'){
+               char small = pq.poll();
+               int index = mp.get(small).pollLast();
+               check[index] = true;
+               check[i] = true;
+            }else{
+                pq.add(ch);
+                mp.putIfAbsent(ch , new ArrayDeque<>());
+                mp.get(ch).add(i);
             }
         }
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (keep[i]) {
-                result.append(s.charAt(i));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< n;i++){
+            if(!check[i]){
+                sb.append(s.charAt(i));
             }
         }
-
-        return result.toString();
+        return sb.toString();
     }
 }
