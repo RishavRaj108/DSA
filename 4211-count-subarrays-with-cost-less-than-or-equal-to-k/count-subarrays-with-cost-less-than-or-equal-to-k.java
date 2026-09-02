8d650563e08d2ee2be46bb4f096b443d9d816079
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-09-02 13:16:14 +0530

    Time: 772 ms (6.90%) | Memory: 171 MB (10.34%) - LeetSync

--- 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.java
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.java
@@ -1,32 +1,23 @@
 class Solution {
     public long countSubarrays(int[] nums, long k) {
         TreeMap<Integer, Integer> mp = new TreeMap<>();
-
         long ans = 0;
         int i = 0;
         int n = nums.length;
-
         for (int j = 0; j < n; j++) {
-
             // Add nums[j]
             mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
-
             // Shrink window while cost > k
             while (1L * (mp.lastKey() - mp.firstKey()) * (j - i + 1) > k) {
-
                 mp.put(nums[i], mp.get(nums[i]) - 1);
-
                 if (mp.get(nums[i]) == 0) {
                     mp.remove(nums[i]);
                 }
-
                 i++;
             }
-
             // All subarrays [i..j], [i+1..j], ... [j..j]
             ans += j - i + 1;
         }
-
         return ans;
     }
 }
