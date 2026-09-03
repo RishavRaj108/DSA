ec248f5d9a2b3a953b233f086c3755f4a451d0a5
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-09-03 10:05:29 +0530

    Time: 661 ms (12.07%) | Memory: 171.1 MB (8.62%) - LeetSync

--- 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.java
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.java
@@ -1,23 +1,25 @@
 class Solution {
     public long countSubarrays(int[] nums, long k) {
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
-        long ans = 0;
-        int i = 0;
         int n = nums.length;
-        for (int j = 0; j < n; j++) {
-            // Add nums[j]
-            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
-            // Shrink window while cost > k
-            while (1L * (mp.lastKey() - mp.firstKey()) * (j - i + 1) > k) {
-                mp.put(nums[i], mp.get(nums[i]) - 1);
-                if (mp.get(nums[i]) == 0) {
-                    mp.remove(nums[i]);
+        TreeMap<Integer , Integer> mp = new TreeMap<>();
+
+        long cnt = 0L;
+        int left = 0;
+
+        for(int right = 0;right < n;right++){
+            int no = nums[right];
+            mp.put(no , mp.getOrDefault(no , 0) + 1);
+
+            while((1L * (mp.lastKey() - mp.firstKey()) * (right - left + 1)) > k){
+                int noL = nums[left];
+                mp.put(noL , mp.get(noL) - 1);
+                if(mp.get(noL) == 0){
+                    mp.remove(noL);
                 }
-                i++;
+                left++;
             }
-            // All subarrays [i..j], [i+1..j], ... [j..j]
-            ans += j - i + 1;
+            cnt += right - left + 1;
         }
-        return ans;
+        return cnt;
     }
 }
