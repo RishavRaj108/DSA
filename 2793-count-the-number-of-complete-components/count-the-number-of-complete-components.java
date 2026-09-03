a571bb91a24aa4c9af81fe1c73cfa057d6c8f978
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-09-03 19:00:58 +0530

    Time: 13 ms (29.08%) | Memory: 47.6 MB (60.28%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -2,29 +2,25 @@ class Solution {
     public int countCompleteComponents(int n, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0;i < n;i++)adj.add(new ArrayList<>());
-
         for(int[] edge : edges){
             int a = edge[0];
             int b = edge[1];
             adj.get(a).add(b);
             adj.get(b).add(a);
         }
-
-        List<List<Integer>> compo = new ArrayList<>();
         boolean[] vis = new boolean[n];
-        
-
+        int cnt = 0;
         for(int i = 0;i < n;i++){
             if(!vis[i]){
-              List<Integer> ls = new ArrayList<>();
               Queue<Integer> q = new LinkedList<>();
               q.add(i);
               vis[i] = true;
-
+              int vertices = 0;
+              int edgesCnt = 0;
               while(!q.isEmpty()){
                 int node = q.poll();
-                
-                ls.add(node);
+                vertices++;
+                edgesCnt += adj.get(node).size();
                 for(int neig : adj.get(node)){
                     if(!vis[neig]){
                         vis[neig] = true;
@@ -32,29 +28,15 @@ class Solution {
                     }
                 }
               }
-              compo.add(ls);
+              edgesCnt = edgesCnt/2;
+              if( (vertices * (vertices - 1))/ 2 == edgesCnt )cnt++;
             }
         }
 
 
-        int cnt = 0;
+        
 
-        for(List<Integer> ls : compo){
-            boolean check = true;
-            
-            for(int i = 0;i < ls.size();i++){
-                for(int j = 0;j < ls.size();j++){
-                    if(i == j)continue;
-                    if(!adj.get(ls.get(j)).contains(ls.get(i))){
-                        check = false;
-                        break;
-                    }
-                }
-                if(!check)break;
-            }
-
-            if(check )cnt++;
-        }
+      
         return cnt;
     }
 }
