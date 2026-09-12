2f56ede13948039cb359ebc8e2855fd77db597ca
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-09-12 10:28:15 +0530

    Time: 165 ms (68.51%) | Memory: 155.6 MB (62.98%) - LeetSync

--- 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.java
+++ 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
-        int[][] arr = new int[n][3];
+        int[][] values = new int[n][3];
         for(int i = 0;i < n;i++){
-            arr[i][0] = i;
-            arr[i][1] = nums1[i];
-            arr[i][2] = nums2[i]; 
-        }
+            values[i][0] = i;
+            values[i][1] = nums1[i];
+            values[i][2] = nums2[i];
+        } 
+        Arrays.sort(values , (a,b) -> a[1] - b[1]);
 
-        Arrays.sort(arr, (a,b) -> a[1] - b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         long[] res = new long[n];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         long sum = 0;
         for(int i = 0;i < n;i++){
-            if(i > 0 && arr[i][1] == arr[i - 1][1]){
-                res[arr[i][0]] = res[arr[i - 1][0]];
+            if(i > 0 && values[i][1] == values[i - 1][1]){
+                res[values[i][0]] = res[values[i - 1][0]];
             }else{
-                res[arr[i][0]] = sum;
+                res[values[i][0]] = sum;
             }
-
-            pq.add(arr[i][2]);
-            sum += arr[i][2];
+            pq.add(values[i][2]);
+            sum += values[i][2];
             if(pq.size() > k){
                 sum -= pq.poll();
             }
