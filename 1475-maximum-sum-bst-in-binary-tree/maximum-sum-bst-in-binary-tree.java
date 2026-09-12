fd8665487cac60a0ce566119aaa085735d7da90b
Rishav Raj <167567737+RishavRaj108@users.noreply.github.com>
2026-09-12 16:18:54 +0530

    Time: 9 ms (11.25%) | Memory: 66.8 MB (23.58%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -1,70 +1,37 @@
 class Solution {
-
     class Tuple {
-        int maxi;
-        int mini;
+        int maxi, mini;
         boolean isBst;
-        int sum;
-
-        Tuple(int maxi, int mini, boolean isBst, int sum) {
+        int sum;    
+        int maxiBst; 
+        Tuple(int maxi, int mini, boolean isBst, int sum, int maxiBst) {
             this.maxi = maxi;
             this.mini = mini;
             this.isBst = isBst;
             this.sum = sum;
+            this.maxiBst = maxiBst;
         }
     }
-
-    int ans = 0;
-
     public int maxSumBST(TreeNode root) {
-        find(root);
-        return ans;
+        return Math.max(find(root).maxiBst, 0);
     }
-
     public Tuple find(TreeNode root) {
-
         if (root == null) {
-            return new Tuple(
-                Integer.MIN_VALUE,
-                Integer.MAX_VALUE,
-                true,
-                0
-            );
+            return new Tuple(Integer.MIN_VALUE, Integer.MAX_VALUE, true, 0, 0);
         }
-
+        if (root.left == null && root.right == null) {
+            return new Tuple(root.val, root.val, true, root.val, root.val);
+        }
         Tuple left = find(root.left);
         Tuple right = find(root.right);
-
-        int maxi = Math.max(root.val,
-                    Math.max(left.maxi, right.maxi));
-
-        int mini = Math.min(root.val,
-                    Math.min(left.mini, right.mini));
-
-        // Current subtree is a BST
-        if (left.isBst &&
-            right.isBst &&
-            root.val > left.maxi &&
-            root.val < right.mini) {
-
-            int sum = left.sum + root.val + right.sum;
-
-            ans = Math.max(ans, sum);
-
-            return new Tuple(
-                maxi,
-                mini,
-                true,
-                sum
-            );
+        int maxi = Math.max(root.val, Math.max(left.maxi, right.maxi));
+        int mini = Math.min(root.val, Math.min(left.mini, right.mini));
+        if (left.isBst && right.isBst && root.val > left.maxi && root.val < right.mini) {
+            int sum = root.val + left.sum + right.sum;       
+            int best = Math.max(sum, Math.max(left.maxiBst, right.maxiBst));
+            return new Tuple(maxi, mini, true, sum, best);
+        } else {
+            return new Tuple(maxi, mini, false, 0, Math.max(left.maxiBst, right.maxiBst));
         }
-
-        // Current subtree is NOT a BST
-        return new Tuple(
-            maxi,
-            mini,
-            false,
-            0
-        );
     }
 }
